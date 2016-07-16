855cbcca25903c9b29289fa741616caedccb8ae6
durack1 <durack1@llnl.gov>
2016-07-15 19:41:23 -0700

    Tables created; Imported coordinates from CMIP6_CVs

--- writeJson.py
+++ writeJson.py
@@ -10,20 +10,28 @@ This script generates all json files residing this this subdirectory
 PJD 12 Jul 2016     - Started
 PJD 13 Jul 2016     - Updated to download existing tables
 PJD 14 Jul 2016     - Successfully loaded dictionaries
-PJD 14 Jul 2016     - TODO:
+PJD 15 Jul 2016     - Tables successfully created, coordinates from CMIP6_CVs
+PJD 15 Jul 2016     - TODO:
 
 @author: durack1
 """
 
 #%% Import statements
-import gc,json,os,ssl,urllib2
+import gc,json,os,ssl,time,urllib2
 #homePath = os.path.join('/','/'.join(os.path.realpath(__file__).split('/')[0:-1]))
 homePath = '/export/durack1/git/obs4MIPs-cmor-tables/'
 print homePath
 os.chdir(homePath)
 
+#%% urllib2 config
+# Create urllib2 context to deal with lab certs
+ctx                 = ssl.create_default_context()
+ctx.check_hostname  = False
+ctx.verify_mode     = ssl.CERT_NONE
+
 #%% List target tables
 masterTargets = [
+ 'coordinate',
  'Amon',
  'Lmon',
  'Omon',
@@ -32,6 +40,7 @@ masterTargets = [
 
 #%% Tables
 tableSource = [
+ 'https://raw.githubusercontent.com/WCRP-CMIP/CMIP6_CVs/master/CMIP6_coordinate.json',
  'https://raw.githubusercontent.com/PCMDI/cmip6-cmor-tables/master/Tables/CMIP6_Amon.json',
  'https://raw.githubusercontent.com/PCMDI/cmip6-cmor-tables/master/Tables/CMIP6_Lmon.json',
  'https://raw.githubusercontent.com/PCMDI/cmip6-cmor-tables/master/Tables/CMIP6_Omon.json',
@@ -39,12 +48,9 @@ tableSource = [
  ] ;
 
 #%% Loop through tables and create in-memory objects
-# Create urllib2 context to deal with lab certs
-ctx                 = ssl.create_default_context()
-ctx.check_hostname  = False
-ctx.verify_mode     = ssl.CERT_NONE
 # Loop through input tables
 for count,table in enumerate(tableSource):
+    print count,masterTargets[count],table
     # Read web file
     jsonOutput                      = urllib2.urlopen(table, context=ctx)
     tmp                             = jsonOutput.read()
@@ -60,44 +66,44 @@ for count,table in enumerate(tableSource):
     del(tmp,jsonOutput)
     del(count,table) ; gc.collect()
 
-#%% Cleanup
+#%% Cleanup by extracting only variable lists
+for table in masterTargets:
+    if table == 'coordinate':
+        pass
+    else:
+        eval(table).pop('axis_entry')
+        eval(table)['Header'].pop('generic_levels')
+        eval(table)['Header']['table_date'] = time.strftime('%d %B %Y')
 
-Amon.get('axis_entry').keys()
-sorted(Amon.get('axis_entry').keys())
-
-#%%
-tmpFile = open('tmp.json','w')
-tmpFile.write(Amon)
-tmpFile.close()
-newDict = json.load(open('tmp.json','r'))
-
-#json.dump(eval(masterTargets[count]),tmpFile,ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
-#json.dump(tmpFile,eval(test),ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
-#tmpFile.close()
-
-
-#%%
-
-experiment  = exps['CV']['experiment_ids'] ;
-# Fix issues
-experiment['1pctCO2Ndep']['experiment']             = '1 percent per year increasing CO2 experiment with increasing N-deposition'
-experiment['omip1'] = experiment.pop('omipv1')
-
+#%% Cleanup realms
+Amon['Header']['realm']     = 'atmos'
+Lmon['Header']['realm']     = 'land'
+Omon['Header']['realm']     = 'ocean'
+SImon['Header']['realm']    = 'seaIce'
 
 #%% Write variables to files
 for jsonName in masterTargets:
-    # Clean formats
-    for key, value in experiment.iteritems():
-        for values in value.iteritems():
-            string = experiment[key][values[0]]
-            string = string.strip() ; # Remove whitespace
-            string = string.strip(',.') ; # Remove trailing characters
-            experiment[key][values[0]] = string.replace(' + ',' and ')  ; # Replace +
+    # Clean experiment formats
+    if jsonName in ['coordinate','experiment_id','grid','formula_terms']:
+        dictToClean = eval(jsonName)
+        for key, value in dictToClean.iteritems():
+            for values in value.iteritems():
+                string = dictToClean[key][values[0]]
+                if not isinstance(string, list):
+                    string = string.strip() ; # Remove trailing whitespace
+                    string = string.strip(',.') ; # Remove trailing characters
+                    string = string.replace(' + ',' and ')  ; # Replace +
+                    string = string.replace(' & ',' and ')  ; # Replace +
+                    string = string.replace('   ',' ') ; # Replace '  ', '   '
+                    string = string.replace('anthro ','anthropogenic ') ; # Replace anthro
+                    string = string.replace('  ',' ') ; # Replace '  ', '   '
+                dictToClean[key][values[0]] = string
+        vars()[jsonName] = dictToClean
     # Write file
-    if 'mip_era' == jsonName:
+    if jsonName == 'mip_era':
         outFile = ''.join(['../',jsonName,'.json'])
     else:
-        outFile = ''.join(['../CMIP6_',jsonName,'.json'])
+        outFile = ''.join(['../obs4MIPs_',jsonName,'.json'])
     # Check file exists
     if os.path.exists(outFile):
         print 'File existing, purging:',outFile
@@ -106,4 +112,6 @@ for jsonName in masterTargets:
     json.dump(eval(jsonName),fH,ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
     fH.close()
 
-     # Validate - only necessary if files are not written by json module
+del(jsonName,outFile) ; gc.collect()
+
+# Validate - only necessary if files are not written by json module
