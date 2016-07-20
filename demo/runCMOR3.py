f587dbae4f207d438a81fc6588224a5afabade50
durack1 <durack1@llnl.gov>
2016-07-20 15:46:46 -0700

    Tweaks to enhance readability - runCMOR3.py

--- demo/runCMOR3.py
+++ demo/runCMOR3.py
@@ -4,9 +4,17 @@
 Created on Mon Jul 18 13:49:08 2016
 
 This script generates all json files required to run obs4MIPs data creation and
-then calls cmor3 to generate two example files
+then calls cmor3 to generate two example files.
+
+The file is split into 3 sections:
+    1 - creates controlled vocabulary (CV) json inputs for CMOR3
+    2 - creates an Omon (ocean monthly) json input for generating a 'tos'
+        variable from a CF-compliant input
+    3 - creates an fx (fixed field) json input for generating an 'areacello'
+        variable from a CF-compliant input
 
 PJD 20 Jul 2016     - Removed source_id
+PJD 20 Jul 2016     - Further tweaks to enhance readability
                     - TODO:
 
 @author: durack1
@@ -18,18 +26,17 @@ import cdms2 as cdm
 import numpy as np
 
 #%% Set local path
-homePath = os.path.join('/','/'.join(os.path.realpath(__file__).split('/')[0:-1]))
-#homePath = '/export/durack1/git/obs4MIPs-cmor-tables/'
-#homePath = '/sync/git/obs4MIPs-cmor-tables/demo'
+homePath = os.path.join('/','/'.join(os.path.realpath(__file__).split('/')[0:-1])) ; # Extract path from executing file
+#homePath = '/export/durack1/git/obs4MIPs-cmor-tables/' ; # Hard code path
+#homePath = '/sync/git/obs4MIPs-cmor-tables/demo' ; # Hard code path
 os.chdir(homePath)
 
-#%% urllib2 config
-# Create urllib2 context to deal with lab certs
+#%% Create urllib2 context to deal with lab/LLNL web certificates
 ctx                 = ssl.create_default_context()
 ctx.check_hostname  = False
 ctx.verify_mode     = ssl.CERT_NONE
 
-#%% Integrate all CVs into master file
+#%% SECTION 1 - Integrate all CVs into master file - create obs4MIPs_CV.json
 jsonCVs = 'obs4MIPs_CV.json'
 buildList = [
  ['frequency','https://raw.githubusercontent.com/PCMDI/obs4MIPs-cmor-tables/master/obs4MIPs_frequency.json'],
@@ -103,7 +110,8 @@ fH = open(outFile,'w')
 json.dump(obs4MIPs_CV,fH,ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
 fH.close()
 
-#%% Integrate Omon into master file
+
+#%% SECTION 2 - Integrate Omon into master file - create obs4MIPs_Omon_composite.json
 jsonOmon = 'obs4MIPs_Omon_composite.json'
 buildList = [
  ['coordinate','https://raw.githubusercontent.com/PCMDI/obs4MIPs-cmor-tables/master/obs4MIPs_coordinate.json'],
@@ -148,7 +156,44 @@ fH = open(outFile,'w')
 json.dump(table,fH,ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
 fH.close()
 
-#%% Integrate fx into master file
+#%% Process variable (with time axis)
+cmor.setup(inpath='../Tables',netcdf_file_action=cmor.CMOR_REPLACE_4)
+cmor.dataset_json('drive_obs4MIPs.json') ; # Update contents of this file to set your global_attributes
+f       = cdm.open('amipobs_tos_360x180_v1.1.0_187001-187112.nc')
+d       = f['tos']
+lat     = d.getLatitude()
+lon     = d.getLongitude()
+time    = d.getTime()
+#cmor.set_cur_dataset_attribute('history',f.history) ; # Force local file attribute as history
+table   = 'obs4MIPs_Omon_composite.json' ; # Amon,Lmon,Omon,SImon
+obs4MIPsOmonID = cmor.load_table(table) ; # Load target table (above), axis info (coordinates, grid*) and CVs
+axes    = [ {'table_entry': 'time',
+             'units': 'days since 1870-01-01',
+             },
+             {'table_entry': 'latitude',
+              'units': 'degrees_north',
+              'coord_vals': lat[:],
+              'cell_bounds': lat.getBounds()},
+             {'table_entry': 'longitude',
+              'units': 'degrees_east',
+              'coord_vals': lon[:],
+              'cell_bounds': lon.getBounds()},
+          ]
+axis_ids = list()
+for axis in axes:
+    axis_id = cmor.axis(**axis)
+    axis_ids.append(axis_id)
+varid   = cmor.variable('tos',d.units,axis_ids)
+values  = np.array(d[:],np.float32)
+cmor.set_deflate(varid,1,1,1) ; # shuffle=1,deflate=1,deflate_level=1 ; CMOR 3.0.6+
+cmor.write(varid,values,time_vals=time[:],time_bnds=time.getBounds()) ; # Write variable with time axis
+f.close()
+cmor.close()
+# Cleanup
+del(f,d,lat,lon,time) ; gc.collect()
+
+
+#%% SECTION 3 - Integrate fx into master file - create obs4MIPs_fx_composite.json
 jsonFx = 'obs4MIPs_fx_composite.json'
 buildList = [
  ['coordinate','https://raw.githubusercontent.com/PCMDI/obs4MIPs-cmor-tables/master/obs4MIPs_coordinate.json'],
@@ -194,42 +239,6 @@ if os.path.exists(outFile):
 fH = open(outFile,'w')
 json.dump(table,fH,ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
 fH.close()
-
-#%% Process variable (with time axis)
-cmor.setup(inpath='../Tables',netcdf_file_action=cmor.CMOR_REPLACE_4)
-cmor.dataset_json('drive_obs4MIPs.json') ; # Update contents of this file to set your global_attributes
-f       = cdm.open('amipobs_tos_360x180_v1.1.0_187001-187112.nc')
-d       = f['tos']
-lat     = d.getLatitude()
-lon     = d.getLongitude()
-time    = d.getTime()
-#cmor.set_cur_dataset_attribute('history',f.history) ; # Force local file attribute as history
-table   = 'obs4MIPs_Omon_composite.json' ; # Amon,Lmon,Omon,SImon
-obs4MIPsOmonID = cmor.load_table(table) ; # Load target table (above), axis info (coordinates, grid*) and CVs
-axes    = [ {'table_entry': 'time',
-             'units': 'days since 1870-01-01',
-             },
-             {'table_entry': 'latitude',
-              'units': 'degrees_north',
-              'coord_vals': lat[:],
-              'cell_bounds': lat.getBounds()},
-             {'table_entry': 'longitude',
-              'units': 'degrees_east',
-              'coord_vals': lon[:],
-              'cell_bounds': lon.getBounds()},
-          ]
-axis_ids = list()
-for axis in axes:
-    axis_id = cmor.axis(**axis)
-    axis_ids.append(axis_id)
-varid   = cmor.variable('tos',d.units,axis_ids)
-values  = np.array(d[:],np.float32)
-cmor.set_deflate(varid,1,1,1) ; # shuffle=1,deflate=1,deflate_level=1 ; CMOR 3.0.6+
-cmor.write(varid,values,time_vals=time[:],time_bnds=time.getBounds()) ; # Write variable with time axis
-f.close()
-cmor.close()
-# Cleanup
-del(f,d,lat,lon,time) ; gc.collect()
 
 #%% Process fixed field
 cmor.setup(inpath='../Tables',netcdf_file_action=cmor.CMOR_REPLACE_4)
