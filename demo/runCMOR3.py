68cace9fe949560945f9d02e31f83f2c823f67ce
durack1 <durack1@llnl.gov>
2017-01-31 10:30:32 -0800

    Successful update to cmor 3.2.1

--- demo/runCMOR3.py
+++ demo/runCMOR3.py
@@ -17,21 +17,22 @@ PJD 20 Jul 2016     - Removed source_id
 PJD 20 Jul 2016     - Further tweaks to enhance readability
 PJD 28 Sep 2016     - Update to deal with new json (embedded key) formats
 PJD 30 Jan 2017     - Update to deal with new tables
+PJD 31 Jan 2017     - Updated to work with CMOR 3.2.1
                     - TODO:
 
 @author: durack1
 """
 
 #%% Import statements
-import cmor,gc,json,os ; #,ssl,sys,urllib2
+import cmor,gc,json,os
 import cdms2 as cdm
 import numpy as np
 from durolib import readJsonCreateDict
 
 #%% Set local path
-#homePath = os.path.join('/','/'.join(os.path.realpath(__file__).split('/')[0:-1])) ; # Extract path from executing file
-#homePath = '/export/durack1/git/obs4MIPs-cmor-tables/' ; # Hard code path
-homePath = '/sync/git/obs4MIPs-cmor-tables/demo' ; # Hard code path
+homePath = os.path.join('/','/'.join(os.path.realpath(__file__).split('/')[0:-1])) ; # Extract path from executing file
+#homePath = '/export/durack1/git/obs4MIPs-cmor-tables/' ; # Linux hard code path
+#homePath = '/sync/git/obs4MIPs-cmor-tables/demo' ; # OS-X hard code path
 os.chdir(homePath)
 
 #%% SECTION 1 - Integrate all controlled vocabularies (CVs) into master file - create obs4MIPs_CV.json
@@ -65,7 +66,6 @@ obs4MIPs_CV['CV'] = {}
 for count,CV in enumerate(buildList):
     CVName1 = CV[0]
     if CVName1 in ['coordinate','formula_terms','grids']:
-        print CV
         continue
     else:
         obs4MIPs_CV['CV'][CVName1] = eval(CVName1)
@@ -76,7 +76,6 @@ outFilePairs = {'obs4MIPs_CV':'obs4MIPs_CV.json',
 
 # Loop through dictionary and write files
 for count,pair in enumerate(outFilePairs):
-    print count,pair
     if os.path.exists(outFilePairs[pair]):
         print 'File existing, purging:',outFilePairs[pair]
         os.remove(outFilePairs[pair])
@@ -90,9 +89,7 @@ del(CVName1,buildList,coordinate,count,formula_terms,frequency,grid_label,homePa
     required_global_attributes,table_id)
 
 #%% SECTION 2 - Integrate Omon into master file - create obs4MIPs_Omon_composite.json
-jsonOmon = 'obs4MIPs_Omon_composite.json'
 buildList = [
- ['coordinate','https://raw.githubusercontent.com/PCMDI/obs4MIPs-cmor-tables/master/obs4MIPs_coordinate.json'],
  ['Omon','https://raw.githubusercontent.com/PCMDI/obs4MIPs-cmor-tables/master/Tables/obs4MIPs_Omon.json']
  ] ;
 
@@ -105,29 +102,21 @@ for count,table in enumerate(tmp.keys()):
         vars()[table] = tmp[table]
 del(tmp,count,table) ; gc.collect()
 
-# Rebuild
-table = {}
-for count,CV in enumerate(buildList):
-    CVName1 = CV[0]
-    if CVName1 == 'coordinate':
-        table['axis_entry'] = eval(CVName1)
-    else:
-        keys = eval(CVName1).keys()
-        for count in range(len(keys)):
-            table[keys[count]] = eval(CVName1).get(keys[count])
-
-outFile = jsonOmon
+outFile = 'obs4MIPs_Omon_composite.json'
 # Check file exists
 if os.path.exists(outFile):
     print 'File existing, purging:',outFile
     os.remove(outFile)
 fH = open(outFile,'w')
-json.dump(table,fH,ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
+json.dump(Omon,fH,ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
 fH.close()
 
 # Process variable (with time axis)
+print 'starting cmor'
 cmor.setup(inpath='../Tables',netcdf_file_action=cmor.CMOR_REPLACE_4)
+print 'cmor.setup complete'
 cmor.dataset_json('drive_obs4MIPs.json') ; # Update contents of this file to set your global_attributes
+print 'cmor.dataset_json complete'
 f       = cdm.open('amipobs_tos_360x180_v1.1.0_187001-187112.nc')
 d       = f['tos']
 lat     = d.getLatitude()
@@ -136,6 +125,7 @@ time    = d.getTime()
 #cmor.set_cur_dataset_attribute('history',f.history) ; # Force local file attribute as history
 table   = 'obs4MIPs_Omon_composite.json' ; # Amon,Lmon,Omon,SImon
 obs4MIPsOmonID = cmor.load_table(table) ; # Load target table (above), axis info (coordinates, grid*) and CVs
+print 'cmor.load_table complete'
 axes    = [ {'table_entry': 'time',
              'units': 'days since 1870-01-01',
              },
@@ -153,6 +143,7 @@ for axis in axes:
     axis_id = cmor.axis(**axis)
     axis_ids.append(axis_id)
 varid   = cmor.variable('tos',d.units,axis_ids)
+print 'cmor.variable complete'
 values  = np.array(d[:],np.float32)
 cmor.set_deflate(varid,1,1,1) ; # shuffle=1,deflate=1,deflate_level=1 ; CMOR 3.0.6+
 cmor.write(varid,values,time_vals=time[:],time_bnds=time.getBounds()) ; # Write variable with time axis
@@ -163,35 +154,27 @@ del(f,d,lat,lon,time) ; gc.collect()
 
 
 #%% SECTION 3 - Integrate fx into master file - create obs4MIPs_fx_composite.json
-jsonFx = 'obs4MIPs_fx_composite.json'
 buildList = [
- ['coordinate','https://raw.githubusercontent.com/PCMDI/obs4MIPs-cmor-tables/master/obs4MIPs_coordinate.json'],
- ['fx','https://raw.githubusercontent.com/PCMDI/obs4MIPs-cmor-tables/master/Tables/obs4MIPs_fx.json']
+ ['Ofx','https://raw.githubusercontent.com/PCMDI/cmip6-cmor-tables/master/Tables/CMIP6_Ofx.json']
  ] ;
 
 # Loop through buildList and create output tables
 tmp = readJsonCreateDict(buildList)
 for count,table in enumerate(tmp.keys()):
-    if table == 'coordinate':
-        vars()[table] = tmp[table].get(table)
-    else:
-        vars()[table] = tmp[table]
+    vars()[table] = tmp[table]
 del(tmp,count,table) ; gc.collect()
 
 # Rebuild
 table = {}
 for count,CV in enumerate(buildList):
     CVName1 = CV[0]
-    if CVName1 == 'coordinate':
-        table['axis_entry'] = eval(CVName1)
-    else:
-        keys = eval(CVName1).keys()
-        for count in range(len(keys)):
-            table[keys[count]] = eval(CVName1).get(keys[count])
+    keys = eval(CVName1).keys()
+    for count in range(len(keys)):
+        table[keys[count]] = eval(CVName1).get(keys[count])
 
 table['Header']['realm'] = 'ocean' ; # Overwrite realm info
 
-outFile = jsonFx
+outFile = 'obs4MIPs_Ofx_composite.json'
 # Check file exists
 if os.path.exists(outFile):
     print 'File existing, purging:',outFile
@@ -209,7 +192,7 @@ lat     = d.getLatitude()
 lon     = d.getLongitude()
 time    = d.getTime()
 #cmor.set_cur_dataset_attribute('history',f.history) ; # Force local file attribute as history
-table   = 'obs4MIPs_fx_composite.json' ; # Amon,Lmon,Omon,SImon
+table   = 'obs4MIPs_Ofx_composite.json' ; # Amon,Lmon,Omon,SImon
 cmor.load_table(table)
 axes    = [  {'table_entry': 'latitude',
               'units': 'degrees_north',
