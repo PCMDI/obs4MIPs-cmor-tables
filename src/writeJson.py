d0d80db1390dea2e0c0fc1c42677bab581e098a8
Peter J. Gleckler <gleckler1@llnl.gov>
2018-03-02 18:45:30 -0800

    ran quickscat

--- src/writeJson.py
+++ src/writeJson.py
@@ -648,12 +648,13 @@ source_id['source_id'][key]['source_label'] = 'AIRS'
 """
 
 key = 'QuikSCAT-v20110531' 
+source_id['source_id'][key] = {}
 source_id['source_id'][key]['source_name'] = 'QuikSCAT'
 source_id['source_id'][key]['release_year'] = '2011'
 source_id['source_id'][key]['source_description'] = 'QuikSCAT winds monthly climatology derived from QuikSCAT L2B using the 2006 model function and processing algorithms.'
 source_id['source_id'][key]['source_version_number'] = 'v20110531'
 source_id['source_id'][key]['institution_id'] = 'National Aeronautics and Space Administration, Jet Propulsion Laboratory'
-source_id['source_id'][key]['region'] = 'Global'
+source_id['source_id'][key]['region'] = ['global']
 source_id['source_id'][key]['source_type'] = 'satellite_retrieval'
 source_id['source_id'][key]['source_variables'] =  ['sfcWind','uas','vas']
 ## derived from RC
