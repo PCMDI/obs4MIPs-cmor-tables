504b5a5bd24ffd03657a111e0ba44b4c42de41c1
Peter Gleckler <gleckler1@gates.llnl.gov>
2021-07-07 11:40:14 -0700

    inst and source_ids py files added

---
+++ src/institution_ids.py
@@ -0,0 +1,16 @@
+institution_id ={}
+institution_id['institution_id'] = {}
+institution_id['institution_id']['ECMWF'] = 'The European Centre for Medium-Range Weather Forecasts, Shinfield Park, Reading RG2 9AX, UK'
+institution_id['institution_id']['MRI'] = 'Meteorological Research Institute, Tsukuba, Ibaraki 305-0052, Japan'
+institution_id['institution_id']['NASA-JPL'] = 'NASA Jet Propulsion Laboratory, Pasadena, CA 91109, USA'
+institution_id['institution_id']['NASA-GSFC'] = 'NASA Goddard Space Flight Center, Greenbelt MD, USA'
+institution_id['institution_id']['NASA/LaRC'] = 'NASA/LaRC (Langley Research Center) Hampton, Va'
+institution_id['institution_id']['NOAA-NCEI'] = 'NOAA National Centers for Environmental Information, Asheville, NC 28801, USA'
+institution_id['institution_id']['NOAA-ESRL/PSD'] = 'NOAA Earth System Research Laboratory, Physical Sciences Division, Boulder, CO 80305, USA'
+institution_id['institution_id']['PCMDI'] = 'Program for Climate Model Diagnosis and Intercomparison, Lawrence Livermore National Laboratory, Livermore, CA 94550, USA'
+institution_id['institution_id']['RSS'] = 'Remote Sensing Systems, Santa Rosa, CA 95401, USA'
+institution_id['institution_id']['DWD'] = 'Deutscher Wetterdienst, Offenbach 63067, Germany'
+institution_id['institution_id']['NCAR'] = 'National Center for Atmospheric Research, Boulder, CO 80307, USA'
+institution_id['institution_id']['ESSO'] = 'Indian National Centre for Ocean Information Services'
+institution_id['institution_id']['MOHC'] = 'Met Office Hadley Centre'
+institution_id['institution_id']['INCOIS-NIO-IPSL'] =  'Joint research colloboration between INCOIS(India), NIO(India) and IPSL(France)'
