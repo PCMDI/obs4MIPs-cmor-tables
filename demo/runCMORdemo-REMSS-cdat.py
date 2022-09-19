df4e21fc92147fc83c21da469634eae29ff21699
gleckler1 <gleckler1@llnl.gov>
2022-09-19 16:21:59 -0700

    adding to demo comment

--- demo/runCMORdemo-REMSS-cdat.py
+++ demo/runCMORdemo-REMSS-cdat.py
@@ -1,16 +1,19 @@
 import cmor
 import cdms2 as cdm
 import numpy as np
+import json
 #cdm.setAutoBounds('on') # Caution, this attempts to automatically set coordinate bounds - please check outputs using this option
 #import pdb ; # Debug statement - import if enabling below
 
 #%% User provided input
-cmorTable = 'Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
-inputJson = 'rssSsmiPrw-input.json' ; # Update contents of this file to set your global_attributes
-inputFilePath = 'rss_ssmi_prw_v06.6-demo.nc'
-inputVarName = 'prw'
+cmorTable = '../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
+inputJson = 'RSS_prw_v07r01.json' ; # Update contents of this file to set your global_attributes
+inputFilePath = 'tpw_v07r01_198801_202112.nc4.nc'
+inputVarName = 'precipitable_water'
 outputVarName = 'prw'
 outputUnits = 'kg m-2'
+RetrievedInfoJson = 'rss-PRW-v07r01_RetrievedInfo.json'
+
 
 ### BETTER IF THE USER DOES NOT CHANGE ANYTHING BELOW THIS LINE...
 #%% Process variable (with time axis)
@@ -19,22 +22,20 @@ f = cdm.open(inputFilePath)
 d = f(inputVarName)
 lat = d.getLatitude()
 lon = d.getLongitude()
-#time = d.getTime() ; # Assumes variable is named 'time', for the demo file this is named 'months'
 time = d.getAxis(0) ; # Rather use a file dimension-based load statement
-
-# Deal with problematic "months since" calendar/time axis
 time_bounds = time.getBounds()
-time_bounds[:,0] = time[:]
-time_bounds[:-1,1] = time[1:]
-time_bounds[-1,1] = time_bounds[-1,0]+1
-time.setBounds(time_bounds)
-del(time_bounds) ; # Cleanup
+
+comment_suffix = ' ***origins before obs4MIPs*** ' 
+download_info_dic =  json.load(open(RetrievedInfoJson))
+for dd in download_info_dic.keys():
+ comment_suffix = comment_suffix + dd + ':' +download_info_dic[dd] + ' ' 
 
 #%% Initialize and run CMOR
 # For more information see https://cmor.llnl.gov/mydoc_cmor3_api/
 cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4) #,logfile='cmorLog.txt')
 cmor.dataset_json(inputJson)
 cmor.load_table(cmorTable)
+cmor.set_cur_dataset_attribute('history',f.history + comment_suffix) 
 #cmor.set_cur_dataset_attribute('history',f.history) ; # Force input file attribute as history
 axes    = [ {'table_entry': 'time',
              'units': time.units, # 'days since 1870-01-01',
@@ -53,7 +54,6 @@ for axis in axes:
     axisId = cmor.axis(**axis)
     axisIds.append(axisId)
 
-#pdb.set_trace() ; # Debug statement
 
 # Setup units and create variable to write using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
 d.units = outputUnits
