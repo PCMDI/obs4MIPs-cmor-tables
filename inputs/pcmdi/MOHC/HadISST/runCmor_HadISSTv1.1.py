a3c3eae663e85ac3a3032ea174b784b925b4bac2
gleckler1 <gleckler1@llnl.gov>
2022-12-05 15:49:52 -0800

    updated pcmdi execute

--- inputs/pcmdi/MOHC/HadISST/runCmor_HadISSTv1.1.py
+++ inputs/pcmdi/MOHC/HadISST/runCmor_HadISSTv1.1.py
@@ -10,7 +10,7 @@ import json
 #import pdb ; # Debug statement - import if enabling below
 
 #%% User provided input
-cmorTable = '../../../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
+cmorTable = '../../../../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
 inputJson = 'HadISSTv1.1.json' ; # Update contents of this file to set your global_attributes
 inputFilePathbgn = '/p/user_pub/pmp/pmp_obs_preparation/orig/data/'
 inputFilePathend = '/HadISSTv1.1/'   #xmls/'
