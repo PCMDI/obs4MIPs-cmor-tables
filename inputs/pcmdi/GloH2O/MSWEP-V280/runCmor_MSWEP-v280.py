fd0518eea3734a38baa089bb59365aede8f368b0
gleckler1 <gleckler1@llnl.gov>
2023-03-22 10:51:25 -0700

    mswep bounds

--- inputs/pcmdi/GloH2O/MSWEP-V280/runCmor_MSWEP-v280.py
+++ inputs/pcmdi/GloH2O/MSWEP-V280/runCmor_MSWEP-v280.py
@@ -8,48 +8,72 @@ import sys, glob
 #%% User provided input
 
 targetgrid = 'orig'
-targetgrid = '2deg'
+#targetgrid = '2deg'
 
-cmorTable = '../../../../Tables/obs4MIPs_A3hr.json' 
+freq = 'Aday'  #'Amon'
+opt = 'Past'
+
+if freq == 'Amon': 
+  cmorTable = '../../../../Tables/obs4MIPs_Amon.json' 
+  avgp = 'Monthly'
+if freq == 'Aday':
+  cmorTable = '../../../../Tables/obs4MIPs_Aday.json'
+  avgp = 'Daily'
+
+
+#if freq == 'day': cmorTable = '../../../../Tables/obs4MIPs_Aday.json'
+#if freq == '3hr': cmorTable = '../../../../Tables/obs4MIPs_A3hr.json'
+
+
+
 
 if targetgrid == 'orig':
-  inputJson = 'CMORPH_V1.0_3hr-input.json' ; 
-  subdir = '3hr.center/'
+  inputJson = 'MSWEP-v280-input.json' ; 
+subdir = opt + '/' + avgp + '/'
 
 if targetgrid == '2deg':
-  inputJson = 'CMORPH_V1.0_3hr-input-250km.json' ;
-  subdir = '3hr.center_2deg/'
+  inputJson = 'MSWEP-v280-input.json' ;
 
-inputFilePathbgn = '/p/user_pub/PCMDIobs/obs4MIPs_input/'
-inputFilePathend = '/NOAA-NCEI/CMOPRH_mahn_v20210719/CMORPH_v1.0/' + subdir
+inputFilePathbgn = '/p/user_pub/PCMDIobs/obs4MIPs_input/GloH2O/MSWEP-V280/MSWEP_V280/' 
+inputFilePathend = subdir
 
 #lstall = glob.glob(inputFilePathbgn+inputFilePathend + '*2002*.nc')
-lstall = glob.glob(inputFilePathbgn+inputFilePathend + '*.nc')
+lstall = glob.glob(inputFilePathbgn+inputFilePathend + '2017*.nc')
 
-#lstall = [inputFilePathbgn+inputFilePathend + 'CMORPH_v1.0_0.25deg_3hr_200201.nc']
+
+def extract_date(ds):   # preprocessing function when opening files
+    for var in ds.variables:
+        if var == 'time':
+            dataset_time = ds[var].values
+            dataset_units = ds[var].units
+            ds.assign(time=dataset_time)
+            ds["time"].attrs = {"units": dataset_units}
+    return ds
+
 
 print(len(lstall),' ', lstall)
 #w = sys.stdin.readline()
 
-inputVarName = 'pr'
+inputVarName = 'precipitation'
 outputVarName = 'pr'
 outputUnits = 'kg m-2 s-1'
 
 ### BETTER IF THE USER DOES NOT CHANGE ANYTHING BELOW THIS LINE...
 #for fi in range(len(inputVarName)):
-for fi in lstall:
-  print(fi)
+#for fi in lstall:
+for a in [1]:
 # f = cdm.open(inputFilePath+inputFileName[fi])
-  f = xr.open_dataset(fi,decode_times=False)
+  f = xr.open_mfdataset(lstall,mask_and_scale=False, decode_times=False, combine='nested', concat_dim='time', preprocess=extract_date, data_vars='all')
   d = f[inputVarName]
   d = np.divide(d,3600.)
+  print('d read')
+  time = f.time
+  lat = f.lat  #.values
+  lon = f.lon  #.values
 
-  lat = f.latitude.values  #f.getLatitude()
-  lon = f.longitude.values  #d.getLongitude()
-  time = f.time.values   #d.get
-
-  if targetgrid == 'orig': f = f.drop_vars(["lat_bounds","lon_bounds"])
-  if targetgrid == '2deg': f = f.drop_vars(["bounds_latitude","bounds_longitude"])
+  time['axis'] = "T"
+  lat['axis'] = "Y"
+  lon['axis'] = "X"
 
   f = f.bounds.add_bounds("X")  #, width=0.5)
   f = f.bounds.add_bounds("Y")  
@@ -58,10 +82,11 @@ for fi in lstall:
 #####time.setBounds() #####time_bounds)
 #####del(time_bounds) ; # Cleanup
 
-# print('above cmor')
+  print('above cmor')
+  w = sys.stdin.readline()
 #%% Initialize and run CMOR
 # For more information see https://cmor.llnl.gov/mydoc_cmor3_api/
-  cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4,logfile='cmorLog.txt')
+  cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4,logfile= 'cmorLog.txt')
   cmor.dataset_json(inputJson)
   cmor.load_table(cmorTable)
 #cmor.set_cur_dataset_attribute('history',f.history) ; # Force input file attribute as history
@@ -71,22 +96,23 @@ for fi in lstall:
              {'table_entry': 'latitude',
               'units': 'degrees_north',
               'coord_vals': lat[:],
-              'cell_bounds': f.latitude_bnds},
+              'cell_bounds': f.lat_bnds},
              {'table_entry': 'longitude',
               'units': 'degrees_east',
               'coord_vals': lon[:],
-              'cell_bounds': f.longitude_bnds},
+              'cell_bounds': f.lon_bnds},
           ]
   axisIds = list() ; # Create list of axes
   for axis in axes:
     axisId = cmor.axis(**axis)
     axisIds.append(axisId)
 
-#pdb.set_trace() ; # Debug statement
 
+  print('above varid')
 # Setup units and create variable to write using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
   varid   = cmor.variable(outputVarName,outputUnits,axisIds,missing_value=1.e20)
   values  = np.array(d[:],np.float32)
+  print('below values')
 
 # Append valid_min and valid_max to variable before writing using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
   #cmor.set_variable_attribute(varid,'valid_min',2.0)
