79ff3aa99c521b172867ae3cf7e26114fe2b18e5
rss_user <rss_user@localhost.localdomain>
2023-03-24 12:11:06 -0700

    TOA CRE variables now available for CERES v4.2. Adjusted code to read them

--- inputs/RSS/NASA-LaRC/runCMOR_CERES4.2_2D-xcdat.py
+++ inputs/RSS/NASA-LaRC/runCMOR_CERES4.2_2D-xcdat.py
@@ -6,19 +6,23 @@ import numpy as np
 #%% User provided input
 cmorTable = '../../../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
 inputJson = 'CERES4.2-input.json' ; # Update contents of this file to set your global_attributes
-inputFilePath = '/home/rss_user/files-obs4MIPs/NASA-LaRC/CERES-EBAF-TOA/' # change to user's path where file is stored
 
-inputFileName = 'CERES_EBAF-TOA_Ed4.2_Subset_200003-202211.nc'
-inputVarName = ['toa_lw_all_mon','toa_sw_all_mon','toa_sw_clr_c_mon','toa_lw_clr_c_mon','toa_net_all_mon','solar_mon'] #v4.2 TOA CRE not available yet as of 02/23
-outputVarName = ['rlut','rsut','rsutcs','rlutcs','rt','rsdt']
-outputUnits = ['W m-2','W m-2','W m-2','W m-2','W m-2','W m-2']
-outpos = ['up','up','up','up','','down']
+inputVarName = ['toa_lw_all_mon','toa_sw_all_mon','toa_sw_clr_c_mon','toa_lw_clr_c_mon','toa_net_all_mon','solar_mon','toa_cre_lw_mon','toa_cre_sw_mon']
+outputVarName = ['rlut','rsut','rsutcs','rlutcs','rt','rsdt','rltcre','rstcre']
+outputUnits = ['W m-2','W m-2','W m-2','W m-2','W m-2','W m-2','W m-2','W m-2']
+outpos = ['up','up','up','up','','down','up','up']
 
-
-### BETTER IF THE USER DOES NOT CHANGE ANYTHING BELOW THIS LINE...
 for fi in range(len(inputVarName)):
 
   print(fi, inputVarName[fi])
+
+  if inputVarName[fi] in ['toa_cre_lw_mon','toa_cre_sw_mon']: 
+    inputFilePath =  '/home/rss_user/files-obs4MIPs/NASA-LaRC/TOA_separatefile/' # change to user's path where file is stored
+    inputFileName = 'CERES_EBAF_Ed4.2_Subset_200003-202203-CRE.nc'
+  else:
+    inputFilePath = '/home/rss_user/files-obs4MIPs/NASA-LaRC/CERES-EBAF-TOA/' # change to user's path where file is stored
+    inputFileName = 'CERES_EBAF-TOA_Ed4.2_Subset_200003-202211.nc'
+### BETTER IF THE USER DOES NOT CHANGE ANYTHING BELOW THIS LINE...
 #%% Process variable (with time axis)
 # Open and read input netcdf file
   f = xc.open_dataset(inputFilePath+inputFileName, decode_times=False, decode_cf=False) # both need to be set to get time units and missing value data
