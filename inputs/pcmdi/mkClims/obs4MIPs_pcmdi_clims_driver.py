96a8a1b6e7d6e14bcb1f1ba37315ac7af1636aa5
gleckler1 <gleckler1@llnl.gov>
2023-05-16 14:55:10 -0700

    corrected monC

--- inputs/pcmdi/mkClims/obs4MIPs_pcmdi_clims_driver.py
+++ inputs/pcmdi/mkClims/obs4MIPs_pcmdi_clims_driver.py
@@ -5,10 +5,8 @@ import datetime
 
 ver_out = datetime.datetime.now().strftime('v%Y%m%d')
 
-
 ver = 'v20230109'
 ver = 'latest'
-
 
 pin = '/p/user_pub/PCMDIobs/obs4MIPs/*/*/mon/*/gn/' + ver 
 
@@ -27,8 +25,8 @@ w = sys.stdin.readline()
 for infile in lst:
 #  infile = os.path.realpath(infile) 
    var = infile.split('/')[8]
-   tmp = infile.replace('/mon/','/Cmon/') 
-   outfile = tmp.replace('_mon_','_Cmon_')
+   tmp = infile.replace('/mon/','/monC/') 
+   outfile = tmp.replace('_mon_','_monC_')
    outfile = outfile.replace(ver,ver_out)
 #  print(outfile)
    cmd = 'pcmdi_compute_climatologies.py -p test_obs_param.py --infile ' + infile + ' --outfile ' + outfile + ' --vars ' + var
