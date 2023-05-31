b445f2e7b81eeabb2b5c2194e0c9272b8dfc69bc
gleckler1 <gleckler1@llnl.gov>
2023-05-31 15:00:18 -0700

    adding avg1

--- inputs/pcmdi/qc/test.py
+++ inputs/pcmdi/qc/test.py
@@ -13,7 +13,8 @@ srcs = ddic[var].keys()
 
 for src in srcs:
   template = '/p/user_pub/PCMDIobs/' + ddic['pr'][src]['template']
-  f = xc.open_dataset(template, decode_times=False, decode_cf=False)
+# f = xc.open_dataset(template, decode_times=False, decode_cf=False)
+  f= xc.open_mfdataset(template, mask_and_scale=True, decode_times=False, combine='nested', concat_dim='time', data_vars='all')
   d = f[var] 
   d0 = d[0]
 # ds_global_avg = d0.spatial.average(var)
