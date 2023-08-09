776c8ef3f1440986929c508a85de4446a2617018
gleckler1 <gleckler1@llnl.gov>
2023-08-08 17:53:24 -0700

    KET's clim

--- inputs/PCMDI/mkClims/obs4MIPs_pcmdi_clims_driver.py
+++ inputs/PCMDI/mkClims/obs4MIPs_pcmdi_clims_driver.py
@@ -6,9 +6,11 @@ import datetime
 ver_out = datetime.datetime.now().strftime('v%Y%m%d')
 
 ver = 'v20230109'
-ver = 'latest'
+#ver = 'latest'
 
 pin = '/p/user_pub/PCMDIobs/obs4MIPs/*/ERA-5/mon/ta/*/' + ver 
+
+pin = '/p/user_pub/PCMDIobs/obs4MIPs/NASA-LaRC/CERES-EBAF-4-1/mon/rlut/gn/' + ver 
 
 lstt = glob.glob(pin + '/*.nc')
 
@@ -25,10 +27,13 @@ print('number of files ',len(lst))
 for infile in lst:
 #  infile = os.path.realpath(infile) 
    var = infile.split('/')[8]
-   tmp = infile.replace('/mon/','/monC/') 
-   outfile = tmp.replace('_mon_','_monC_')
+   tmp = infile.replace('/obs4MIPs/','/obs4MIPs_tmp/') 
+   outfile = tmp
    outfile = outfile.replace(ver,ver_out)
 #  print(outfile)
    cmd = 'pcmdi_compute_climatologies.py -p test_obs_param.py --infile ' + infile + ' --outfile ' + outfile + ' --vars ' + var
    os.popen(cmd).readlines()
    print(cmd)
+
+# obs4MIPs/REMSS-PRW-v07r01/mon/sfcWind/100km/climatology/v20230520
+# sfcWind-tclm-h010-hxy-x_mon_REMSS-PRW-v07r01_PCMDI_gn_20001-202212.nc
