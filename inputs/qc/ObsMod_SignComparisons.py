32f63bcd6c7ff75c6bc1f2c42a6f22292369b5ae
Peter Gleckler <gleckler1@gates.llnl.gov>
2023-11-29 11:22:51 -0800

    adding ceres

--- inputs/qc/ObsMod_SignComparisons.py
+++ inputs/qc/ObsMod_SignComparisons.py
@@ -1,18 +1,23 @@
 import xcdat as xc
 
+varz = ['rlut','rsut','rsdt','rsutcs','rlutcs','rsds','rsus','rsuscs','rsdscs','rldscs']
 
-varz = ['rlut','rsut']
-
-obs_template = ''
-
+obs_template = '/p/user_pub/PCMDIobs/obs4MIPs/NASA-LaRC/CERES-EBAF-4-2/output/obs4MIPs/NASA-LaRC/CERES-EBAF-4-2/mon/VAR/gn/v20231122/VAR_mon_CERES-EBAF-4-2_RSS_gn_200003-202307.nc'
 mod1_template = '/p/css03/esgf_publish/CMIP6/CMIP/CCCma/CanESM5-1/historical/r1i1p1f1/Amon/VAR/gn/v20190429/VAR_Amon_CanESM5-1_historical_r1i1p1f1_gn_185001-201412.nc' 
-
 
 for vr in varz:
    mod1 = mod1_template.replace('VAR',vr)
    mds1 = xc.open_dataset(mod1)
    m1 = mds1.isel(time=slice(0,1))[vr].squeeze()
    mavg = float(mds1.isel(time=3).spatial.average(vr,axis=['X','Y'])[vr].compute())
-   print(vr,' ',str(mavg)) 
+
+   try:
+    obs = obs_template.replace('VAR',vr)
+    o1 = xc.open_dataset(obs)
+    o = o1.isel(time=slice(0,1))[vr].squeeze()
+    oavg = float(o1.isel(time=3).spatial.average(vr,axis=['X','Y'])[vr].compute())
+   except:
+    oavg = 'N/A'
+   print(vr,' ',str(mavg),'  ',oavg) 
 
 
