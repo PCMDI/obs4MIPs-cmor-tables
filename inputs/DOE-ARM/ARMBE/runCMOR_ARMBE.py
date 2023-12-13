c7a7d543c347b981e7d92ecad03e6bac173a735e
chengzhuzhang <zhang40@llnl.gov>
2023-12-13 13:09:17 -0800

    add armbe cmor files

---
+++ inputs/DOE-ARM/ARMBE/runCMOR_ARMBE.py
@@ -0,0 +1,67 @@
+import cmor
+import xarray as xr
+import xcdat as xc
+import numpy as np
+import json
+
+#%% User provided input
+# TODO don't have a currently support obs4MIPs table for hourly site specific data
+cmorTable = '../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
+inputJson = 'ARMBE_ATM.json' ; # Update contents of this file to set your global_attributes
+inputFilePath = '/Users/zhang40/Documents/ARM/armbe_sample/sgparmbeatmC1.c1.20200101.003000.nc'
+inputVarName = 'precip_rate_sfc' # Unit is mm/hour
+outputVarName = 'pr'
+outputUnits = 'kg m-2 s-1'
+
+### USER MAY NOT NEED TO CHANGE ANYTHING BELOW THIS LINE...
+
+# Open and read input netcdf file
+f = xr.open_dataset(inputFilePath,decode_times=False)
+d = f[inputVarName]
+lat = f.lat.values 
+lon = f.lon.values 
+time = f.time.values ; # Rather use a file dimension-based load statement
+f = f.bounds.add_bounds("X")  
+f = f.bounds.add_bounds("Y")  
+f = f.bounds.add_bounds("T")
+# CONVERT UNITS FROM mm/hour to kg/m2/s 
+d = np.divide(d,86400.*24.)
+d = np.where(np.isnan(d),1.e20,d)
+
+#%% Initialize and run CMOR
+# For more information see https://cmor.llnl.gov/mydoc_cmor3_api/
+cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4) #,logfile='cmorLog.txt')
+cmor.dataset_json(inputJson)
+cmor.load_table(cmorTable)
+cmor.set_cur_dataset_attribute('history',f.history) 
+
+axes    = [ {'table_entry': 'time',
+             'units': f.time.units, # 'days since 1870-01-01',
+             },
+             {'table_entry': 'latitude',
+              'units': 'degrees_north',
+              'coord_vals': lat[:],
+              'cell_bounds': f.lat_bnds},
+             {'table_entry': 'longitude',
+              'units': 'degrees_east',
+              'coord_vals': lon[:],
+              'cell_bounds': f.lon_bnds},
+          ]
+axisIds = list() ; # Create list of axes
+for axis in axes:
+    axisId = cmor.axis(**axis)
+    axisIds.append(axisId)
+
+# Setup units and create variable to write using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
+varid   = cmor.variable(outputVarName,outputUnits,axisIds,missing_value=1.e20)
+values  = np.array(d[:],np.float32)
+
+# Append valid_min and valid_max to variable before writing using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
+cmor.set_variable_attribute(varid,'valid_min','f',2.0)
+cmor.set_variable_attribute(varid,'valid_max','f',3.0)
+
+# Prepare variable for writing, then write and close file - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
+cmor.set_deflate(varid,1,1,1) ; # shuffle=1,deflate=1,deflate_level=1 - Deflate options compress file data
+cmor.write(varid,values,time_vals=time[:],time_bnds=f.time_bnds.values) ; # Write variable with time axis
+f.close()
+cmor.close()
