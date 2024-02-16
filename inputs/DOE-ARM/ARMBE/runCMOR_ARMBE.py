e00ecaf5fde608ead1b65fda5f609b79ec899f80
Peter Gleckler <gleckler1@gates.llnl.gov>
2024-02-16 15:20:58 -0800

    testing cmor

--- inputs/DOE-ARM/ARMBE/runCMOR_ARMBE.py
+++ inputs/DOE-ARM/ARMBE/runCMOR_ARMBE.py
@@ -6,9 +6,10 @@ import json
 
 #%% User provided input
 # TODO don't have a currently support obs4MIPs table for hourly site specific data
-cmorTable = '../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
+cmorTable = '../../../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
 inputJson = 'ARMBE_ATM.json' ; # Update contents of this file to set your global_attributes
-inputFilePath = '/Users/zhang40/Documents/ARM/armbe_sample/sgparmbeatmC1.c1.20200101.003000.nc'
+#inputFilePath = '/Users/zhang40/Documents/ARM/armbe_sample/sgparmbeatmC1.c1.20200101.003000.nc'
+inputFilePath = '/p/user_pub/PCMDIobs/obs4MIPs_input/LLNL/ARMBE_Vxy/sgparmbeatmC1.c1.20180101.003000.nc'
 inputVarName = 'precip_rate_sfc' # Unit is mm/hour
 outputVarName = 'pr'
 outputUnits = 'kg m-2 s-1'
@@ -21,8 +22,8 @@ d = f[inputVarName]
 lat = f.lat.values 
 lon = f.lon.values 
 time = f.time.values ; # Rather use a file dimension-based load statement
-f = f.bounds.add_bounds("X")  
-f = f.bounds.add_bounds("Y")  
+#f = f.bounds.add_bounds("X")  
+#f = f.bounds.add_bounds("Y")  
 f = f.bounds.add_bounds("T")
 # CONVERT UNITS FROM mm/hour to kg/m2/s 
 d = np.divide(d,86400.*24.)
@@ -30,30 +31,19 @@ d = np.where(np.isnan(d),1.e20,d)
 
 #%% Initialize and run CMOR
 # For more information see https://cmor.llnl.gov/mydoc_cmor3_api/
-cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4) #,logfile='cmorLog.txt')
+cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4,logfile='cmorLog.txt')
 cmor.dataset_json(inputJson)
 cmor.load_table(cmorTable)
 cmor.set_cur_dataset_attribute('history',f.history) 
 
-axes    = [ {'table_entry': 'time',
-             'units': f.time.units, # 'days since 1870-01-01',
-             },
-             {'table_entry': 'latitude',
-              'units': 'degrees_north',
-              'coord_vals': lat[:],
-              'cell_bounds': f.lat_bnds},
-             {'table_entry': 'longitude',
-              'units': 'degrees_east',
-              'coord_vals': lon[:],
-              'cell_bounds': f.lon_bnds},
-          ]
-axisIds = list() ; # Create list of axes
-for axis in axes:
-    axisId = cmor.axis(**axis)
-    axisIds.append(axisId)
+cmorLat = cmor.axis("latitude", coord_vals=np.array([lat]), units="degrees_north")
+cmorLon = cmor.axis("longitude", coord_vals=np.array([lon]), units="degrees_east")
+cmorTime = cmor.axis("time", coord_vals=time[:], cell_bounds=tbds, units= f.time.units)
+cmoraxes = [cmorTime,cmorLat, cmorLon]
+
 
 # Setup units and create variable to write using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
-varid   = cmor.variable(outputVarName,outputUnits,axisIds,missing_value=1.e20)
+varid   = cmor.variable(outputVarName,outputUnits,cmoraxes,missing_value=1.e20)
 values  = np.array(d[:],np.float32)
 
 # Append valid_min and valid_max to variable before writing using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
