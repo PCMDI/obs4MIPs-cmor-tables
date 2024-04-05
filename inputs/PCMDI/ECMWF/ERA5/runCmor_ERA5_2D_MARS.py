6b1e9739c3b5cbd3a4e6473e36da6b0533bb1392
Peter Gleckler <gleckler1@gates.llnl.gov>
2024-04-05 16:10:27 -0700

    ERA5 to ODS2.5

--- inputs/PCMDI/ECMWF/ERA5/runCmor_ERA5_2D_MARS.py
+++ inputs/PCMDI/ECMWF/ERA5/runCmor_ERA5_2D_MARS.py
@@ -1,18 +1,22 @@
 import cmor
-import cdms2 as cdm
+import xcdat as xc
+import xarray as xr
+from xarray.coding.times import encode_cf_datetime
 import numpy as np
-import cdutil
-cdm.setAutoBounds('on') # Caution, this attempts to automatically set coordinate bounds - please check outputs using this option
-#import pdb ; # Debug statement - import if enabling below
+import cftime
+import sys,os
+
+sys.path.append("../../../misc/") # Path to obs4MIPsLib
+import obs4MIPsLib
 
 #%% User provided input
-cmorTable = '../../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
+cmorTable = '../../../../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
 inputJson = 'ERA5-MARS-input.json' ; # Update contents of this file to set your global_attributes
-inputFilePathbgn = '/p/user_pub/pmp/pmp_obs_preparation/orig/data/'
-inputFilePathend = '/ERA5/fromMARS/'
+inputFilePathbgn = '/p/user_pub/PCMDIobs/obs4MIPs_input/ECMWF/'
+inputFilePathend = 'ERA5/fromMARS/'
 
 inputFileName = ['adaptor.mars.internal-1580171536.0444872-8315-37-02e9201d-5e7b-41b5-98ff-3b9b3a83d82d.nc','adaptor.mars.internal-1580171536.0444872-8315-37-02e9201d-5e7b-41b5-98ff-3b9b3a83d82d.nc','adaptor.mars.internal-1580171536.0444872-8315-37-02e9201d-5e7b-41b5-98ff-3b9b3a83d82d.nc','adaptor.mars.internal-1580190896.023251-25621-38-f188c480-a5cd-4284-84a7-5757315ca044.nc']
-inputVarName = ['t2m_0001','u10_0001','v10_0001','msl_0001'] 
+inputVarName = ['t2m_0001'] #,'u10_0001','v10_0001','msl_0001'] 
 outputVarName = ['tas','uas','vas','psl']  
 outputUnits = ['K','m s-1','m s-1','Pa'] 
 outpos = ['','','',''] 
@@ -29,59 +33,40 @@ outpos = ['down','down']
 for fi in range(len(inputVarName)):
   print(fi, inputVarName[fi])
   inputFilePath = inputFilePathbgn+inputFilePathend
-#%% Process variable (with time axis)
 # Open and read input netcdf file
-  f = cdm.open(inputFilePath+inputFileName[fi])
-  d = f(inputVarName[fi])(squeeze=1)
-  lat = d.getLatitude()
-  lon = d.getLongitude()
+  f = xc.open_dataset(inputFilePath+inputFileName[fi],decode_times=False, decode_cf=False)
+  f = f.bounds.add_missing_bounds(axes=['X', 'Y'])
+  f = f.bounds.add_bounds('T')
+  d = f[inputVarName[fi]].values
+  lat = f.latitude
+  lon = f.longitude
   print(d.shape)
-#time = d.getTime() ; # Assumes variable is named 'time', for the demo file this is named 'months'
-  time = d.getAxis(0) ; # Rather use a file dimension-based load statement
-
-  time_bounds = time.getBounds()
-
-  d.positive = outpos[fi]
-
-#####time.setBounds() #####time_bounds)
-#####del(time_bounds) ; # Cleanup
+  time = f.time.values[:]
+# d.positive = outpos[fi]
 
 #%% Initialize and run CMOR
-# For more information see https://cmor.llnl.gov/mydoc_cmor3_api/
   cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4) #,logfile='cmorLog.txt')
   cmor.dataset_json(inputJson)
   cmor.load_table(cmorTable)
 #cmor.set_cur_dataset_attribute('history',f.history) ; # Force input file attribute as history
-  axes    = [ {'table_entry': 'time',
-             'units': time.units, # 'days since 1870-01-01',
-             },
-             {'table_entry': 'latitude',
-              'units': 'degrees_north',
-              'coord_vals': lat[:],
-              'cell_bounds': lat.getBounds()},
-             {'table_entry': 'longitude',
-              'units': 'degrees_east',
-              'coord_vals': lon[:],
-              'cell_bounds': lon.getBounds()},
-          ]
-  axisIds = list() ; # Create list of axes
-  for axis in axes:
-    axisId = cmor.axis(**axis)
-    axisIds.append(axisId)
 
-
+  cmorLat = cmor.axis("latitude", coord_vals=lat[:].values, cell_bounds=f.latitude_bnds.values, units="degrees_north")
+  cmorLon = cmor.axis("longitude", coord_vals=lon[:].values, cell_bounds=f.longitude_bnds.values, units="degrees_east")
+  cmorTime = cmor.axis("time", coord_vals=time, cell_bounds=f.time_bnds.values, units= f.time.units)
+  axes = [cmorTime, cmorLat, cmorLon]
 # Setup units and create variable to write using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
-  d.units = outputUnits[fi]
-  varid   = cmor.variable(outputVarName[fi],d.units,axisIds,missing_value=d.missing,positive=d.positive)
+  varid   = cmor.variable(outputVarName[fi],outputUnits[fi],axes,missing_value=1.e20)
   values  = np.array(d[:],np.float32)
 
-# Append valid_min and valid_max to variable before writing using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
-  #cmor.set_variable_attribute(varid,'valid_min',2.0)
-  #cmor.set_variable_attribute(varid,'valid_max',3.0)
+# Provenance info - produces global attribute <obs4MIPs_GH_Commit_ID> 
+  gitinfo = obs4MIPsLib.ProvenanceInfo(obs4MIPsLib.getGitInfo("./"))
+  paths = os.getcwd().split('/inputs')
+  path_to_code = f"/inputs{paths[1]}"  # location of the code in the obs4MIPs GitHub directory
+  full_git_path = f"https://github.com/PCMDI/obs4MIPs-cmor-tables/tree/{gitinfo['commit_number']}/{path_to_code}"
+  cmor.set_cur_dataset_attribute("processing_code_location",f"{full_git_path}")
 
 # Prepare variable for writing, then write and close file - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
   cmor.set_deflate(varid,1,1,1) ; # shuffle=1,deflate=1,deflate_level=1 - Deflate options compress file data
-  cmor.write(varid,values,time_vals=time[:],time_bnds=time_bounds) ; # Write variable with time axis
+  cmor.write(varid,values) ; # Write variable with time axis
   f.close()
-
   cmor.close()
