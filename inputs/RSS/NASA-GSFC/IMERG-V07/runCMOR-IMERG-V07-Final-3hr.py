Lat/lon fix and 3-hour averaging checks implemented

inputs/RSS/NASA-GSFC/IMERG-V07/runCMOR-IMERG-V07-Final-3hr.py
@@ -3,6 +3,8 @@ import cmor
 import xcdat as xc
 import numpy as np
 import glob
+import datetime
+import calendar
 from make_IMERG_3hr import avg_3hr_imerg
 import sys
 import os
@@ -20,12 +22,39 @@ inputVarName = 'pr'
 outputVarName = 'pr'
 outputUnits = 'kg m-2 s-1'
 
-
-for year in range(2001, 2002):  # put the years you want to process here
+files_list = sorted(glob.glob(os.path.join(inputFilePath,f'*.HDF5')))
+for year in range(2000, 2024):  # put the years you want to process here
     inputDatasets = []
-    for month in range(1,2):
+    for month in range(1,13):
 
+        # Must make sure the day before and the day after a given month exist otherwise the 3-hourly averaging code will break
+        previous_date = datetime.datetime(year,month,1) - datetime.timedelta(days=1)
+        previous_year = previous_date.year
+        previous_month = previous_date.month
+        previous_day = previous_date.day
 
+        res = calendar.monthrange(year, month)
+        last_day = res[1]
+
+        future_date = datetime.datetime(year,month,last_day) + datetime.timedelta(days=1)
+        future_year = future_date.year
+        future_month = future_date.month
+        future_day = future_date.day
+
+        yyyymmdd_prev = f"{previous_year}{str(previous_month).zfill(2)}{str(previous_day).zfill(2)}"
+        yyyymmdd_futr = f"{future_year}{str(future_month).zfill(2)}{str(future_day).zfill(2)}"
+
+        
+        valid_files_prev = [file for file in files_list if yyyymmdd_prev in file]
+        valid_files_futr = [file for file in files_list if yyyymmdd_futr in file]
+
+        if valid_files_prev == []:
+            print(f"No files for {previous_year}-{str(previous_month).zfill(2)}-{str(previous_day).zfill(2)}. Averaging unavailable")
+            continue
+        elif valid_files_futr == []:
+            print(f"No files for {future_year}-{str(future_month).zfill(2)}-{str(future_day).zfill(2)}. Averaging unavailable")
+            continue
+        
         print(f'Starting averaging for {year}-{month:02}')
         # Function to average IMERG data to 3-hourly maps.  'ext' is the file extension.  This code will
         # work for 'RT-H5', 'HDF5'/'H5', 'nc', and 'nc4' file extensions.  'RT-H5' for the sake of this demo.
@@ -36,15 +65,23 @@ for year in range(2001, 2002):  # put the years you want to process here
         inputDataset = inputDataset.bounds.add_bounds("T")
 
         d = inputDataset[inputVarName]
-        time = inputDataset.time
-        lat = inputDataset.lat.values
-        lon = inputDataset.lon.values
 
-        # Due to CMOR warnings related to the way latitudes and longitudes are read in/rounded
-        # need to round lat and lon bounds to 3 places after the decimal
-        lat_bounds = np.around(inputDataset.lat_bnds, 3)
-        lon_bounds = np.around(inputDataset.lon_bnds, 3)
+        time = inputDataset.time
         time_bounds = inputDataset.time_bnds
+
+        # The following lines are written to address floating point representation errors within the IMERG lat/lon data
+        lat = ['{:g}'.format(float('{:.4g}'.format(i))) for i in inputDataset.lat.values]
+        lon = ['{:g}'.format(float('{:.5g}'.format(i))) for i in inputDataset.lon.values]
+        lat_bounds = ['{:g}'.format(float('{:.4g}'.format(i))) for i in np.ravel(inputDataset.lat_bnds.values)]
+        lon_bounds = ['{:g}'.format(float('{:.5g}'.format(i))) for i in np.ravel(inputDataset.lon_bnds.values)]
+        
+        lat = np.asarray(lat,dtype=float)
+        lon = np.asarray(lon,dtype=float)
+        lat_bounds = np.reshape(np.asarray(lat_bounds,dtype=float), (1800,2))
+        lon_bounds = np.reshape(np.asarray(lon_bounds,dtype=float), (3600,2))
+
+        lat_bounds[(lat_bounds > -0.1) & (lat_bounds < 0.1)] = 0.0
+        lon_bounds[(lon_bounds > -0.1) & (lon_bounds < 0.1)] = 0.0
 
 
         #%% Initialize and run CMOR
@@ -97,4 +134,3 @@ for year in range(2001, 2002):  # put the years you want to process here
         cmor.write(varid,values,time_vals=time.values[:],time_bnds=time_bounds.values[:]) ; # Write variable with time axis
         inputDataset.close()
         cmor.close()
-        sys.exit()

inputs/RSS/NASA-GSFC/IMERG-V07/runCMOR-IMERG-V07-Final.py
@@ -28,9 +28,9 @@ inputVarName = 'precipitation'
 outputVarName = 'pr'
 outputUnits = 'kg m-2 s-1'
 
-for year in range(2001, 2002):  # put the years you want to process here
+for year in range(2000, 2024):  # put the years you want to process here
     inputDatasets = []
-    for month in range(1,2):
+    for month in range(1,13):
         inputFiles = glob.glob(f"{inputFilePath}/3B-HHR.MS.MRG.3IMERG.{year}{month:02}*.HDF5")
         inputFiles.sort() # to ensure data files are in chronological order. Code will break otherwise
         for inputFile in inputFiles:
@@ -48,14 +48,21 @@ for year in range(2001, 2002):  # put the years you want to process here
         d = d.transpose('time','lat','lon') # need to transpose the IMEG latitudes and longitudes
 
         time = f.time
-        lat = f.lat.values
-        lon = f.lon.values
-
-        # Due to CMOR warnings related to the way latitudes and longitudes are read in/rounded
-        # need to round lat and lon bounds to 3 places after the decimal
-        lat_bounds = np.around(f.lat_bnds, 3)
-        lon_bounds = np.around(f.lon_bnds, 3)
         time_bounds = f.time_bnds
+
+        # The following lines are written to address floating point representation errors within the IMERG lat/lon data
+        lat = ['{:g}'.format(float('{:.4g}'.format(i))) for i in f.lat.values]
+        lon = ['{:g}'.format(float('{:.5g}'.format(i))) for i in f.lon.values]
+        lat_bounds = ['{:g}'.format(float('{:.4g}'.format(i))) for i in np.ravel(f.lat_bnds.values)]
+        lon_bounds = ['{:g}'.format(float('{:.5g}'.format(i))) for i in np.ravel(f.lon_bnds.values)]
+        
+        lat = np.asarray(lat,dtype=float)
+        lon = np.asarray(lon,dtype=float)
+        lat_bounds = np.reshape(np.asarray(lat_bounds,dtype=float), (1800,2))
+        lon_bounds = np.reshape(np.asarray(lon_bounds,dtype=float), (3600,2))
+
+        lat_bounds[(lat_bounds > -0.1) & (lat_bounds < 0.1)] = 0.0
+        lon_bounds[(lon_bounds > -0.1) & (lon_bounds < 0.1)] = 0.0
 
         #%% Initialize and run CMOR
         # For more information see https://cmor.llnl.gov/mydoc_cmor3_api/
@@ -107,4 +114,3 @@ for year in range(2001, 2002):  # put the years you want to process here
         f.close()
         cmor.close()
         print(f"File written for {year}-{month:02}")
-        sys.exit()