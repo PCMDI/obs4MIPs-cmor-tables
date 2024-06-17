5a0c3b914f6a1477471bfbcb28794f4476cdc58e
Peter Gleckler <gleckler1@gates.llnl.gov>
2024-06-17 16:17:33 -0700

    3hr working

--- inputs/PCMDI/GloH2O/MSWEP-V280/runCmor_MSWEP-v280_3hr.py
+++ inputs/PCMDI/GloH2O/MSWEP-V280/runCmor_MSWEP-v280_3hr.py
@@ -8,7 +8,7 @@ import numpy as np
 import sys, os, glob
 
 freq = 'A3hr' #'Amon' #'Aday'  #'Amon'
-version = 'Past-nogauge'  # 'Past'  #'Past-nogauge'  #'Past'  # NRT   # Past-nogauge 
+version = 'Past'  # 'Past'  #'Past-nogauge'  #'Past'  # NRT   # Past-nogauge 
 cmorTable = '../../../../Tables/obs4MIPs_A3hr.json'
 avgp = '3hourly'
 inputJson = 'MSWEP-V280-' + version + '_input.json' ; 
@@ -44,23 +44,28 @@ outputUnits = 'kg m-2 s-1'
 lstyrs = ['1979', '1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
 #lstyrs = ['1981']
 
+mos = ['01','02','03','04','05','06','07','08','09','10','11','12']
+
 for yr in lstyrs:  # LOOP OVER YEARS
-   print('starting ', yr)
-   pathind = '/p/user_pub/PCMDIobs/obs4MIPs_input/GloH2O/MSWEP-V280/MSWEP_V280/' + version + '/' + avgp + '/' + yr + '*.nc'
-   fc = xc.open_mfdataset(pathind, mask_and_scale=False, decode_times=False, combine='nested', concat_dim='time', preprocess=extract_date, data_vars='all')
-   fc = fc.bounds.add_missing_bounds(axes=['X', 'Y'])
-   fc = fc.bounds.add_bounds('T')
-   tunits = "hours since 1900-1-1 00:00:00"  #fc.time.units
-   tdc = np.multiply(fc.time.values[:],24)  # days-since to hours-since
-   tbds =np.multiply(fc.time_bnds.values[:],24)
-   ddc = fc[inputVarName].values
+  print('starting ', yr)
+  pathind = '/p/user_pub/PCMDIobs/obs4MIPs_input/GloH2O/MSWEP-V280/MSWEP_V280/' + version + '/' + avgp + '/' + yr + '*.nc'
+  fc = xc.open_mfdataset(pathind, mask_and_scale=False, decode_times=True, combine='nested', concat_dim='time', preprocess=extract_date, data_vars='all')
+  fc = fc.bounds.add_missing_bounds(axes=['X', 'Y'])
+  fc = fc.bounds.add_bounds('T')
+  tunits = "days since 1900-1-1 00:00:00"  #fc.time.units
+  lat = fc.lat
+  lon = fc.lon   #.values
+  lat['axis'] = "Y"
+  lon['axis'] = "X"
+
+  for mo in mos:
+
+   fcc = fc.sel(time=slice(yr + "-" + mo,yr + "-" + mo))
+   tdc = fcc.time.values[:]  # days-since to hours-since
+   tbds =fcc.time_bnds.values[:]
+   ddc = fcc[inputVarName].values
    conv = 3600.*24./8.  # 3hrly
    d = np.divide(ddc,conv)
-   lat = fc.lat
-   lon = fc.lon   #.values
-
-   lat['axis'] = "Y"
-   lon['axis'] = "X"
 
    cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4,logfile= 'cmorLog.txt')
    cmor.dataset_json(inputJson)
@@ -69,7 +74,7 @@ for yr in lstyrs:  # LOOP OVER YEARS
 
    cmorLat = cmor.axis("latitude", coord_vals=lat.values, cell_bounds=fc.lat_bnds.values, units="degrees_north")
    cmorLon = cmor.axis("longitude", coord_vals=lon.values, cell_bounds=fc.lon_bnds.values, units="degrees_east")
-   cmorTime = cmor.axis("time", coord_vals=tdc, cell_bounds=tbds, units= tunits)
+   cmorTime = cmor.axis("time", coord_vals= cftime.date2num(tdc,tunits), cell_bounds=cftime.date2num(tbds,tunits), units= tunits)
    axes = [cmorTime, cmorLat, cmorLon]
 # Setup units and create variable to write using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
    varid   = cmor.variable(outputVarName,outputUnits,axes,missing_value=1.e20)
@@ -80,6 +85,6 @@ for yr in lstyrs:  # LOOP OVER YEARS
    cmor.set_deflate(varid,1,1,1) ; # shuffle=1,deflate=1,deflate_level=1 - Deflate options compress file data
    cmor.write(varid,values) ; # Write variable with time axis
    cmor.close()
-   print('done cmorizing ', yr)
+   print('done cmorizing ', yr,' ',mo)
    fc.close()
 
--- inputs/PCMDI/GloH2O/MSWEP-V280/runCmor_MSWEP-v280_mon-day.py
+++ inputs/PCMDI/GloH2O/MSWEP-V280/runCmor_MSWEP-v280_mon-day.py
@@ -9,10 +9,8 @@ import sys, os, glob
 sys.path.append("../../../misc") # Path to obs4MIPsLib used to trap provenance
 import obs4MIPsLib
 
-
-
-freq = 'Aday' #'Amon' #'Aday'  #'A3hr'
-version = 'Past'  #-nogauge'  # 'Past'  #'Past-nogauge'  #'Past'  # NRT   # Past-nogauge 
+freq = 'A3hr' #'Amon' #'Aday'  #'A3hr'
+version = 'Past-nogauge'  # 'Past'  #'Past-nogauge'  #'Past'  # NRT   # Past-nogauge 
 
 if freq == 'Amon': avgp = 'Monthly'
 if freq == 'A3hr': avgp = '3hourly'
@@ -53,39 +51,48 @@ outputUnits = 'kg m-2 s-1'
 lstyrs = ['1979', '1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
 #lstyrs = ['1981']
 
+mos = ['01']
+if freq == 'A3hr': mos = ['01','02','03','04','05','06','07','08','09','10','11','12']
+
 for yr in lstyrs:  # LOOP OVER YEARS
-   print('starting ', yr)
+  print('starting ', yr)
 #  yr = yr  # + '00*' #day    # '001 for 3hr'
-   pathind = '/p/user_pub/PCMDIobs/obs4MIPs_input/GloH2O/MSWEP-V280/MSWEP_V280/' + version + '/' + avgp + '/' + yr + '*.nc'
-   fc = xc.open_mfdataset(pathind, mask_and_scale=False, decode_times=False, combine='nested', concat_dim='time', preprocess=extract_date, data_vars='all')
-   fc = fc.bounds.add_missing_bounds(axes=['X', 'Y'])
-   fc = fc.bounds.add_bounds('T')
+  pathind = '/p/user_pub/PCMDIobs/obs4MIPs_input/GloH2O/MSWEP-V280/MSWEP_V280/' + version + '/' + avgp + '/' + yr + '*.nc'
+  fc = xc.open_mfdataset(pathind, mask_and_scale=False, decode_times=False, combine='nested', concat_dim='time', preprocess=extract_date, data_vars='all')
+  tunits = fc.time.units
+  lat = fc.lat
+  lon = fc.lon
+  lat['axis'] = "Y"
+  lon['axis'] = "X"
+
+  if freq == 'A3hr': fc = xc.open_mfdataset(pathind, mask_and_scale=False, decode_times=True, combine='nested', concat_dim='time', preprocess=extract_date, data_vars='all')
+  fc = fc.bounds.add_missing_bounds(axes=['X', 'Y'])
+  fc = fc.bounds.add_bounds('T')
 #  tunits = "hours since 1900-1-1 00:00:00"  #fc.time.units
-#  tunits = "days since 1900-1-1"
-   tunits = fc.time.units
-   if freq == 'Amon': conv = 3600.*24.*30.4  #OK?
-   if freq == 'Aday': conv = 3600.*24.   #OK
-   if freq == 'A3hr': conv = 3600.*24./8. #OK
-   tdc = fc.time.values[:]
-   tdc = tdc.astype(float) 
-   tbds =fc.time_bnds.values[:]
+  if freq == 'Amon': conv = 3600.*24.*30.4  #OK?
+  if freq == 'Aday': conv = 3600.*24.   #OK
+  if freq == 'A3hr': conv = 3600.*24./8. #OK
+
+  for mo in mos:
+   fcc = fc
+   if freq == 'A3hr': fcc = fc.sel(time=slice(yr + "-" + mo,yr + "-" + mo))
+
+   tdc = fcc.time.values[:]
+   if freq != 'A3hr': tdc = tdc.astype(float) 
+   tbds =fcc.time_bnds.values[:]
 
 #  tdc = np.multiply(fc.time.values[:],1) #,24)  # days-since to hours-since
 #  tbds =np.multiply(fc.time_bnds.values[:],1) #,24)  #added last 2 for monthly
-   ddc = fc[inputVarName].values
+   ddc = fcc[inputVarName].values
    d = np.divide(ddc,conv)
-   lat = fc.lat
-   lon = fc.lon   
-   lat['axis'] = "Y"
-   lon['axis'] = "X"
 
    cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4,logfile= 'cmorLog.txt')
    cmor.dataset_json(inputJson)
    cmor.load_table(cmorTable)
 #cmor.set_cur_dataset_attribute('history',f.history) ; # Force input file attribute as history
 
-   cmorLat = cmor.axis("latitude", coord_vals=lat.values, cell_bounds=fc.lat_bnds.values, units="degrees_north")
-   cmorLon = cmor.axis("longitude", coord_vals=lon.values, cell_bounds=fc.lon_bnds.values, units="degrees_east")
+   cmorLat = cmor.axis("latitude", coord_vals=lat.values, cell_bounds=fcc.lat_bnds.values, units="degrees_north")
+   cmorLon = cmor.axis("longitude", coord_vals=lon.values, cell_bounds=fcc.lon_bnds.values, units="degrees_east")
    cmorTime = cmor.axis("time", coord_vals=tdc, cell_bounds=tbds, units= tunits)
    axes = [cmorTime, cmorLat, cmorLon]
 # Setup units and create variable to write using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
@@ -103,6 +110,7 @@ for yr in lstyrs:  # LOOP OVER YEARS
    cmor.set_deflate(varid,1,1,1) ; # shuffle=1,deflate=1,deflate_level=1 - Deflate options compress file data
    cmor.write(varid,values) ; # Write variable with time axis
    cmor.close()
-   print('done cmorizing ', yr)
-   fc.close()
+   print('done cmorizing ', yr,' ', mo)
+
+  fc.close()
 #  w = sys.stdin.readline()
