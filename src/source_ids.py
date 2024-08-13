19dabec0caad3fdc1fedd608d6862f1d86da55e1
Peter Gleckler <gleckler1@gates.llnl.gov>
2024-08-13 12:38:13 -0700

    SGP source id added

--- src/source_ids.py
+++ src/source_ids.py
@@ -13,6 +13,29 @@ source_id['source_id'][key]['source_type'] = 'insitu'
 source_id['source_id'][key]['source_variables'] = ['uas','vas','ps','pr','hfss','hfls','tas','hfss','hfls','ta','ua','va','hur','wap']
 source_id['source_id'][key]['release_year'] = '2022'
 
+key = 'SGP_ARMBE-atm-c1-1-8'
+source_id['source_id'][key] = {}
+source_id['source_id'][key]['source_name'] = 'ARMBE'
+source_id['source_id'][key]['source_label'] = 'ARMBE'
+source_id['source_id'][key]['source_description'] = 'DOE ARM Best Estimate Data Products for Atmosphere and Cloud properties'
+source_id['source_id'][key]['source_version_number'] = 'atm-c1-1-8'
+source_id['source_id'][key]['institution_id'] = 'DOE-ARM'
+source_id['source_id'][key]['region'] = ['site_data']
+source_id['source_id'][key]['source_type'] = 'insitu'
+source_id['source_id'][key]['source_variables'] = ['uas','vas','ps','pr','hfss','hfls','tas','hfss','hfls','ta','ua','va','hur','wap']
+source_id['source_id'][key]['release_year'] = '2022'
+
+key = 'ARMBE-atm-c1-1-8'
+source_id['source_id'][key] = {}
+source_id['source_id'][key]['source_name'] = 'ARMBE'
+source_id['source_id'][key]['source_label'] = 'ARMBE'
+source_id['source_id'][key]['source_description'] = 'DOE ARM Best Estimate Data Products for Atmosphere and Cloud properties'
+source_id['source_id'][key]['source_version_number'] = 'atm-c1-1-8'
+source_id['source_id'][key]['institution_id'] = 'DOE-ARM'
+source_id['source_id'][key]['region'] = ['site_data']
+source_id['source_id'][key]['source_type'] = 'insitu'
+source_id['source_id'][key]['source_variables'] = ['uas','vas','ps','pr','hfss','hfls','tas','hfss','hfls','ta','ua','va','hur','wap']
+source_id['source_id'][key]['release_year'] = '2022'
 
 key = 'ARMBE-cldrad-c1-1-8'
 source_id['source_id'][key] = {}
