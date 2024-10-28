dfb888c594a4b8f369a0c25a4d4230033d5308a0
Peter J Gleckler <u7510@login20.chn.perlmutter.nersc.gov>
2024-10-28 08:38:16 -0700

    S-E updated

--- inputs/PCMDI/project_downscaling/STAR-ESDM/STAR-ESDM_ACCESS-CM2_runCMOR.py
+++ inputs/PCMDI/project_downscaling/STAR-ESDM/STAR-ESDM_ACCESS-CM2_runCMOR.py
@@ -5,10 +5,8 @@ import cftime
 import numpy as np
 import sys, os, glob
 
-
 cmorTable = '../Tables/obs4MIPs_Aday.json'
 inputJson = 'STAR-ESDM_ACCESS-CM2_input.json'
-
 inputFilePath = '/global/cfs/projectdirs/m3522/cmip6/STAR-ESDM/ssp245/downscaled.ACCESS-CM2.r1i1p1f1.pr.ssp245.gn.nclimgrid.star.1950.2100.tllc.nc'
 inputVarName = 'pr'
 outputVarName = 'pr'
@@ -18,19 +16,24 @@ exps = ['historical','ssp245', 'ssp585']
 mods = ['MPI-ESM1-2-HR', 'TaiESM1', 'CMCC-ESM2', 'ACCESS-ESM1-5', 'MRI-ESM2-0', 'FGOALS-g3', 'NorESM2-MM', 'CanESM5', 'MIROC6', 'ACCESS-CM2', 'NorESM2-LM', 'MPI-ESM1-2-LR', 'BCC-CSM2-MR', 'NESM3']
 
 tmp0 = inputFilePath.split('.')
-print(tmp[0])
+print(tmp0[0])
 
 yrs_hist = [('1950','1954'),('1955','1959'),('1960','1964'),('1965','1969'),('1970','1974'),('1975','1979'),('1980','1984'),('1985','1989'),('1990','1994'),('1995','1999'),('2000','2004'),('2005','2009'),('2010','2014')]
 yrs_scen = [('2015', '2019'), ('2020', '2024'), ('2025', '2029'), ('2030', '2034'), ('2035', '2039'), ('2040', '2044'), ('2045', '2049'), ('2050', '2054'), ('2055', '2059'), ('2060', '2064'), ('2065', '2069'), ('2070', '2074'), ('2075', '2079'), ('2080', '2084'), ('2085', '2089'), ('2090', '2094'), ('2095', '2099')]
 
-
-#yrs = [yrs[0]]
-
 for mod in mods:
- for exp in exps:    
+ for exp in exps:
+  rns= [] 
+  tmp1 = inputFilePath.replace('ACCESS-CM2',mod)
+  tmp2 = tmp1.replace('r1i1p1f1','*')
+  lst = glob.glob(tmp2)
+  for rn in lst:
+   rns.append(rn.split('.')[2])
+  for ri in rns:
     if exp == 'historical': yrs = yrs_hist
     if exp in ['ssp245', 'ssp585']: yrs = yrs_scen
-    fi = inputFilePath
+    fi = inputFilePath.replace('ACCESS-CM2',mod)
+    fi = fi.replace('r1i1p1f1',ri)
     fc = xc.open_dataset(fi,decode_times=False,use_cftime=False)
     fc.time.attrs['calendar'] = '365_day'
     fd = fc
@@ -48,16 +51,16 @@ for mod in mods:
      f = f.bounds.add_bounds("Y")
      f = f.bounds.add_bounds("T")
 
+# For more information see https://cmor.llnl.gov/mydoc_cmor3_api/
+     cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4,logfile='cmorLog.txt')
+     cmor.dataset_json(inputJson)
+     cmor.load_table(cmorTable)
+
 # SET CMIP MODEL SPECIFIC ATTRIBUTES 
      cmor.set_cur_dataset_attribute("source_id","STAR-ESDM-v0--" + mod)
      cmor.set_cur_dataset_attribute("driving_source_id",mod)
      cmor.set_cur_dataset_attribute("driving_variant_label",ri)
      cmor.set_cur_dataset_attribute("driving_experiment_id",exp)
-
-# For more information see https://cmor.llnl.gov/mydoc_cmor3_api/
-     cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4,logfile='cmorLog.txt')
-     cmor.dataset_json(inputJson)
-     cmor.load_table(cmorTable)
 
      time_np = cftime.date2num(time,tunits)
      time_np = np.array(time_np[:],np.float32)  #time_np.astype(np.float32)  
