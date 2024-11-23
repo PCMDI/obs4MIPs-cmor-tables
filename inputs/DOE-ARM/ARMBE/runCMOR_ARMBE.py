a878d5602f597c6f52097b28fa17c53fda6f559c
Peter Gleckler <gleckler1@gates.llnl.gov>
2024-11-22 16:26:25 -0800

    metadata updates

--- inputs/DOE-ARM/ARMBE/runCMOR_ARMBE.py
+++ inputs/DOE-ARM/ARMBE/runCMOR_ARMBE.py
@@ -8,7 +8,7 @@ import json
 # TODO don't have a currently support obs4MIPs table for hourly site specific data
 #cmorTable = '../../../Tables/obs4MIPs_CFsubhr.json' #Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
 #cmorTable = './obs4MIPs_A1hr.json'
-cmorTable = '../../../../Tables/obs4MIPs_A1hr.json'
+cmorTable = '../../../Tables/obs4MIPs_A1hr.json'
 inputJson = 'ARMBE_ATM.json' ; # Update contents of this file to set your global_attributes
 #inputFilePath = '/Users/zhang40/Documents/ARM/armbe_sample/sgparmbeatmC1.c1.20200101.003000.nc'
 inputFilePath = '/p/user_pub/PCMDIobs/obs4MIPs_input/LLNL/ARMBE_Vxy/sgparmbeatmC1.c1.20180101.003000.nc'
@@ -38,8 +38,8 @@ cmor.dataset_json(inputJson)
 cmor.load_table(cmorTable)
 cmor.set_cur_dataset_attribute('history',f.history) 
 
-cmorLat = cmor.axis("latitude", coord_vals=np.array([lat]), units="degrees_north")
-cmorLon = cmor.axis("longitude", coord_vals=np.array([lon]), units="degrees_east")
+cmorLat = cmor.axis("latitude1", coord_vals=np.array([lat]), units="degrees_north")
+cmorLon = cmor.axis("longitude1", coord_vals=np.array([lon]), units="degrees_east")
 cmorTime = cmor.axis("time", coord_vals=time[:], cell_bounds=tbds, units= f.time.units)
 cmoraxes = [cmorTime, cmorLat, cmorLon]
 
