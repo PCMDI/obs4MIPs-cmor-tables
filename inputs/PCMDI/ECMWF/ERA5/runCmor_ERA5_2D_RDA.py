4341a57682acb85513a5c7c1f70d10c128de72ad
Peter Gleckler <gleckler1@gates.llnl.gov>
2024-12-16 11:40:28 -0800

    fixing time

--- inputs/PCMDI/ECMWF/ERA5/runCmor_ERA5_2D_RDA.py
+++ inputs/PCMDI/ECMWF/ERA5/runCmor_ERA5_2D_RDA.py
@@ -8,6 +8,7 @@ import sys,os,glob
 
 sys.path.append("../../../misc/") # Path to obs4MIPsLib
 import obs4MIPsLib
+import fix_dataset_time
 
 #%% User provided input
 cmorTable = '../../../../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx
@@ -15,20 +16,10 @@ inputJson = 'ERA5-MARS-input.json' ; # Update contents of this file to set your 
 inputFilePathbgn = '/p/user_pub/PCMDIobs/obs4MIPs_input/ECMWF/'
 inputFilePathend = 'ERA5/RDA/'
 
-#inputFileName = ['adaptor.mars.internal-1580171536.0444872-8315-37-02e9201d-5e7b-41b5-98ff-3b9b3a83d82d.nc','adaptor.mars.internal-1580171536.0444872-8315-37-02e9201d-5e7b-41b5-98ff-3b9b3a83d82d.nc','adaptor.mars.internal-1580171536.0444872-8315-37-02e9201d-5e7b-41b5-98ff-3b9b3a83d82d.nc','adaptor.mars.internal-1580190896.023251-25621-38-f188c480-a5cd-4284-84a7-5757315ca044.nc']
-
-inputVarName = ['MSL']
+inputVarName = ['VAR_2T'] # ['MSL']
 #inputVarName = ['T','U','V','Z'] 
-outputVarName = ['psl']  
-outputUnits = ['Pa'] 
-
-'''
-inputFileName = ['adaptor.mars.internal-1582240823.3820484-23715-16-fe2f1d48-67a6-479f-9d83-6356ce7cbecb.nc','adaptor.mars.internal-1582240823.3820484-23715-16-fe2f1d48-67a6-479f-9d83-6356ce7cbecb.nc']
-inputVarName = ['ewss', 'nsss']
-outputVarName = ['tauu','tauv']
-outputUnits = ['Pa','Pa']
-outpos = ['down','down']
-'''
+outputVarName = ['tas']  #['psl']  
+outputUnits = ['K']  #['Pa'] 
 
 ### BETTER IF THE USER DOES NOT CHANGE ANYTHING BELOW THIS LINE..
 for vn,fi in enumerate(inputVarName):
@@ -39,7 +30,7 @@ for vn,fi in enumerate(inputVarName):
  files_yearly.sort()
 
  for fi in files_yearly:
-  f = xc.open_dataset(fi,decode_times=False, decode_cf=False)
+  f = xc.open_dataset(fi,decode_times=True, decode_cf=True)
   f = f.bounds.add_missing_bounds(axes=['X', 'Y'])
   f = f.bounds.add_bounds('T')
   d = f[inputVarName[vn]].values
@@ -47,11 +38,23 @@ for vn,fi in enumerate(inputVarName):
   lon = f.longitude
   print(d.shape)
   time = f.time.values[:]
+  attsin = f.attrs
 # d.positive = outpos[fi]
+  print('above wait')
 # w = sys.stdin.readline()
+
+  datumyr = 1979
+  datum_start_month = 1 
+  start_month = 1 
+  end_month = 12
+# tunits = 'days since ' str(datumyr) + '-01-01 00:00:00'
+  yrs = [time[1].year]
+  t, tbds, tunits = fix_dataset_time.monthly_times(datumyr, yrs, datum_start_month, start_month,end_month)
 
 #%% Initialize and run CMOR
   cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4) #,logfile='cmorLog.txt')
+  for origatt in attsin.keys():
+     cmor.set_cur_dataset_attribute(origatt,attsin[origatt])
   cmor.dataset_json(inputJson)
   cmor.load_table(cmorTable)
 #cmor.set_cur_dataset_attribute('history',f.history) ; # Force input file attribute as history
@@ -59,7 +62,7 @@ for vn,fi in enumerate(inputVarName):
   cmorLat = cmor.axis("latitude", coord_vals=lat[:].values, cell_bounds=f.latitude_bnds.values, units="degrees_north")
   cmorLon = cmor.axis("longitude", coord_vals=lon[:].values, cell_bounds=f.longitude_bnds.values, units="degrees_east")
 # cmorLev = cmor.axis("plev37-ERA5", coord_vals=lev[:].values*100., units="Pa")
-  cmorTime = cmor.axis("time", coord_vals=time, cell_bounds=f.time_bnds.values, units= f.time.units)
+  cmorTime = cmor.axis("time", coord_vals=t, cell_bounds=tbds, units= tunits)
   axes = [cmorTime, cmorLat, cmorLon]
 # Setup units and create variable to write using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
   varid   = cmor.variable(outputVarName[vn],outputUnits[vn],axes,missing_value=1.e20)
