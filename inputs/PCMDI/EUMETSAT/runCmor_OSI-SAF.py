c703c97a3e6f7aa29769619826895730b89baff0
Peter Gleckler <gleckler1@gates.llnl.gov>
2025-01-13 16:10:44 -0800

    starting cmor

--- inputs/PCMDI/EUMETSAT/runCmor_OSI-SAF.py
+++ inputs/PCMDI/EUMETSAT/runCmor_OSI-SAF.py
@@ -6,35 +6,41 @@ import numpy as np
 import cftime
 import sys,os,glob
 
-sys.path.append("../../../misc/") # Path to obs4MIPsLib
+sys.path.append("../../misc/") # Path to obs4MIPsLib
 import obs4MIPsLib
 import fix_dataset_time
 
 #%% User provided input
-cmorTable = '../../../../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx
+cmorTable = '../../../Tables/obs4MIPs_SImon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx
 inputJson = 'OSI-SAF_input.json' ; # Update contents of this file to set your global_attributes
-inputFilePathbgn = '/p/user_pub/PCMDIobs/obs4MIPs_input/EUMETSAT/OSI-SAF-450-a-3-0/v20231201/'
+inputFilePathbgn = '/p/user_pub/PCMDIobs/obs4MIPs_input/EUMETSAT/OSI-SAF-450-a-3-0/'
 inputFilePathend = 'v20231201/'
 
-inputVarName = ['ice_conc']
-outputVarName = ['sic']    
-outputUnits = ['%'] 
+inputVarName = 'ice_conc'
+outputVarName = 'sic'    
+outputUnits = '%' 
+
+yrr = range(1978,2020)
+yrs = list(yrr)
 
 ### BETTER IF THE USER DOES NOT CHANGE ANYTHING BELOW THIS LINE..
-for vn,fi in enumerate(inputVarName):
- print(fi, outputVarName[vn])
+#for vn,fi in enumerate(inputVarName):
+for yr in yrs[0:1]: 
 #w = sys.stdin.readline()
- inputFilePath = inputFilePathbgn+inputFilePathend 
- files_yearly = glob.glob(inputFilePath + '*.nc')
+ inputFilePath = inputFilePathbgn+inputFilePathend + '*1979*.nc' 
+ files_yearly = glob.glob(inputFilePath)
  files_yearly.sort()
+ fi = inputFilePath
 
- for fi in files_yearly:
-  f = xc.open_dataset(fi,decode_times=True, decode_cf=True)
+#for fi in files_yearly:
+ for yr in yrs[0:1]:
+         
+  f = xc.open_mfdataset(fi,decode_times=True, decode_cf=True)
   f = f.bounds.add_missing_bounds(axes=['X', 'Y'])
   f = f.bounds.add_bounds('T')
-  d = f[inputVarName[vn]].values
-  lat = f.latitude
-  lon = f.longitude
+  d = f[inputVarName].values
+# lat = f.latitude
+# lon = f.longitude
   print(d.shape)
   time = f.time.values[:]
   attsin = f.attrs
@@ -51,12 +57,29 @@ for vn,fi in enumerate(inputVarName):
   t, tbds, tunits = fix_dataset_time.monthly_times(datumyr, yrs, datum_start_month, start_month,end_month)
 
 #%% Initialize and run CMOR
-  cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4 ,logfile='cmorLog.' + outputVarName[vn] + '.' + str(yrs[0]) +'.txt')
+  cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4 ,logfile='cmorLog.' + outputVarName + '.' + str(yrs[0]) +'.txt')
   for origatt in attsin.keys():
      cmor.set_cur_dataset_attribute(origatt,attsin[origatt])
   cmor.dataset_json(inputJson)
   cmor.load_table(cmorTable)
 #cmor.set_cur_dataset_attribute('history',f.history) ; # Force input file attribute as history
+
+  grid_table_id = cmor.load_table('../../../Tables/obs4MIPs_grids.json')
+  cmor.set_table(grid_table_id)
+
+  grid = gen_irreg_grid(d.shape[1], d.shape[2])
+
+  y_axis_id = cmor.axis(table_entry='y_deg', units='degrees',
+                          coord_vals=grid.y, cell_bounds=grid.y_bnds)
+  x_axis_id = cmor.axis(table_entry='x_deg', units='degrees',
+                          coord_vals=grid.x, cell_bounds=grid.x_bnds)
+
+  grid_id = cmor.grid(axis_ids=[y_axis_id, x_axis_id],
+                        latitude=grid.lat,
+                        longitude=grid.lon,
+                        latitude_vertices=grid.lat_bnds,
+                        longitude_vertices=grid.lon_bnds)
+
 
   cmorLat = cmor.axis("latitude", coord_vals=lat[:].values, cell_bounds=f.latitude_bnds.values, units="degrees_north")
   cmorLon = cmor.axis("longitude", coord_vals=lon[:].values, cell_bounds=f.longitude_bnds.values, units="degrees_east")
