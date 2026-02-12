9fd83a23a055670244613d1926e1edfa9070a7f9
Peter J Gleckler <u7510@login20.chn.perlmutter.nersc.gov>
2026-02-12 15:11:12 -0800

    daily OLR CDR added

--- src/source_ids.py
+++ src/source_ids.py
@@ -13,80 +13,14 @@ source_id['source_id'][key]['region'] = ['global_land']
 source_id['source_id'][key]['source_type'] = 'satellite_blended'
 source_id['source_id'][key]['source_variables'] = ['sweLut']
 
-
-key = 'JASMES-SNOWCOVER-1-0'
+key = 'NOAA-NCEI-CDR-OLR-v02r00'
 source_id['source_id'][key] = {}
-source_id['source_id'][key]['source_name'] = 'JASMES Snow Cover'
-source_id['source_id'][key]['source_label'] = 'JASMES Snow Cover'
-source_id['source_id'][key]['release_year'] = '2024'
-source_id['source_id'][key]['source_description'] = 'Global daily surface snow area fraction derived from merged satellite observations using a consistent objective snow cover mapping algorithm applied to multiple generations of optical sensors since 1978.'
-source_id['source_id'][key]['source_version_number'] = '1-0'
-source_id['source_id'][key]['institution_id'] = 'JAXA'
+source_id['source_id'][key]['source_name'] = 'NOAA-NCEI CDR OLR'
+source_id['source_id'][key]['source_label'] = 'NOAA-NCEI CDR OLR'
+source_id['source_id'][key]['release_year'] = '2014'
+source_id['source_id'][key]['source_description'] = 'NOAA-NCEI CDR Outgoing Longwave Radiation'
+source_id['source_id'][key]['source_version_number'] = 'v02r00'
+source_id['source_id'][key]['institution_id'] = 'NOAA-NCEI'
 source_id['source_id'][key]['region'] = ['global']
 source_id['source_id'][key]['source_type'] = 'satellite_blended'
-source_id['source_id'][key]['source_variables'] = ['snc']
-
-key = 'LIVNEH-1-0'
-source_id['source_id'][key] = {}
-source_id['source_id'][key]['source_name'] = 'LIVNEH'
-source_id['source_id'][key]['source_label'] = 'LIVNEH'
-source_id['source_id'][key]['release_year'] = '2021'
-source_id['source_id'][key]['source_description'] = 'Livneh daily CONUS near-surface gridded meteorological and derived hydrometeorological data data provided by the NOAA PSL'
-source_id['source_id'][key]['source_version_number'] = '1.0'
-source_id['source_id'][key]['institution_id'] = 'NOAA-ESRL-PSD'
-source_id['source_id'][key]['region'] = ['north_america']
-source_id['source_id'][key]['source_type'] = 'gridded_insitu'
-source_id['source_id'][key]['source_variables'] = ['pr','tasmin', 'tasmax','sfcWind']
-
-key = 'LIVNEH-1-0'
-source_id['source_id'][key] = {}
-source_id['source_id'][key]['source_name'] = 'LIVNEH'
-source_id['source_id'][key]['source_label'] = 'LIVNEH'
-source_id['source_id'][key]['release_year'] = '2021'
-source_id['source_id'][key]['source_description'] = 'Livneh daily CONUS near-surface gridded meteorological and derived hydrometeorological data data provided by the NOAA PSL'
-source_id['source_id'][key]['source_version_number'] = '1.0'
-source_id['source_id'][key]['institution_id'] = 'NOAA-ESRL-PSD'
-source_id['source_id'][key]['region'] = ['north_america']
-source_id['source_id'][key]['source_type'] = 'gridded_insitu'
-source_id['source_id'][key]['source_variables'] = ['pr','tasmin', 'tasmax','sfcWind']
-source_id['source_id'][key]['source_version_number'] = '1.0'
-
-key = 'LIVNEH-unsplit-1-0'
-source_id['source_id'][key] = {}
-source_id['source_id'][key]['source_name'] = 'LIVNEH_unsplit' 
-source_id['source_id'][key]['source_label'] = 'LIVNEH_unsplit'
-source_id['source_id'][key]['release_year'] = '2021'
-source_id['source_id'][key]['source_description'] = 'Livneh daily CONUS unsplit'
-source_id['source_id'][key]['source_version_number'] = '1.0'
-source_id['source_id'][key]['institution_id'] = 'UCSD-SIO'
-source_id['source_id'][key]['region'] = ['north_america']
-source_id['source_id'][key]['source_type'] = 'gridded_insitu'
-source_id['source_id'][key]['source_variables'] = ['pr','tasmin', 'tasmax','sfcWind'] 
-
-key = 'CERES-EBAF-4-2-1'
-source_id['source_id'][key] = {}
-source_id['source_id'][key]['source_name'] = "CERES-EBAF4-2" 
-source_id['source_id'][key]['source_label'] = "CERES-EBAF4-2" 
-source_id['source_id'][key]['release_year'] = '2025'
-source_id['source_id'][key]['source_description'] = 'CERES EBAF (Energy Balanced and Filled) TOA Fluxes. Monthly Averages'
-source_id['source_id'][key]['source_version_number'] = '4.2.1'
-source_id['source_id'][key]['institution_id'] = 'NASA-LaRC'
-source_id['source_id'][key]['region'] = ['global']
-source_id['source_id'][key]['source_type'] = 'satellite_blended'
-source_id['source_id'][key]['source_variables'] = ['rlut','rlutcs','rsut','rsutcs']
-
-key = 'GPCP-3-3'
-source_id['source_id'][key] = {}
-source_id['source_id'][key]['source_name'] = "GPCP-3-3"
-source_id['source_id'][key]['source_label'] = "GPCP-3-3"
-source_id['source_id'][key]['release_year'] = '2025'
-source_id['source_id'][key]['source_description'] = 'Global Precipitation Climatology Project (GPCP) Merged Precipitation'
-source_id['source_id'][key]['source_version_number'] = '3.3'
-source_id['source_id'][key]['institution_id'] = 'NASA-LaRC'
-source_id['source_id'][key]['region'] = ['global']
-source_id['source_id'][key]['source_type'] = 'satellite_blended'
-source_id['source_id'][key]['source_variables'] = ['pr']
-
-
-
-
+source_id['source_id'][key]['source_variables'] = ['rlut']
