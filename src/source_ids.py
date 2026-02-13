2fe6df4d9be964350f25773f7e8aeeac73a8687f
Peter J Gleckler <u7510@login20.chn.perlmutter.nersc.gov>
2026-02-13 14:34:32 -0800

    updated sourceids

--- src/source_ids.py
+++ src/source_ids.py
@@ -13,14 +13,27 @@ source_id['source_id'][key]['region'] = ['global_land']
 source_id['source_id'][key]['source_type'] = 'satellite_blended'
 source_id['source_id'][key]['source_variables'] = ['sweLut']
 
-key = 'NOAA-NCEI-CDR-OLR-v02r00'
+key = 'NOAA-NCEI-OISST-v2-1'
 source_id['source_id'][key] = {}
-source_id['source_id'][key]['source_name'] = 'NOAA-NCEI CDR OLR'
-source_id['source_id'][key]['source_label'] = 'NOAA-NCEI CDR OLR'
-source_id['source_id'][key]['release_year'] = '2014'
-source_id['source_id'][key]['source_description'] = 'NOAA-NCEI CDR Outgoing Longwave Radiation'
-source_id['source_id'][key]['source_version_number'] = 'v02r00'
+source_id['source_id'][key]['source_name'] = 'NOAA-NCEI OISST v2.1'
+source_id['source_id'][key]['source_label'] = 'NOAA-NCEI OISST v2.1'
+source_id['source_id'][key]['release_year'] = '2020'
+source_id['source_id'][key]['source_description'] = 'NOAA-NCEI Optimum Interpolation Sea Surface Temperature (OISST)'
+source_id['source_id'][key]['source_version_number'] = 'v2.1'
 source_id['source_id'][key]['institution_id'] = 'NOAA-NCEI'
-source_id['source_id'][key]['region'] = ['global']
+source_id['source_id'][key]['region'] = ['global_ocean']
 source_id['source_id'][key]['source_type'] = 'satellite_blended'
-source_id['source_id'][key]['source_variables'] = ['rlut']
+source_id['source_id'][key]['source_variables'] = ['tos']
+
+key = 'NOAA-NCEI-ERSSTv6'
+source_id['source_id'][key] = {}
+source_id['source_id'][key]['source_name'] = 'NOAA-NCEI ERSSTv6'
+source_id['source_id'][key]['source_label'] = 'NOAA-NCEI ERSSTv6'
+source_id['source_id'][key]['release_year'] = '2025'
+source_id['source_id'][key]['source_description'] = 'NOAA-NCEI Extended Reconstructed Sea Surface Temperature (ERSST)'
+source_id['source_id'][key]['source_version_number'] = 'v6'
+source_id['source_id'][key]['institution_id'] = 'NOAA-NCEI'
+source_id['source_id'][key]['region'] = ['global_ocean']
+source_id['source_id'][key]['source_type'] = 'satellite_blended'
+source_id['source_id'][key]['source_variables'] = ['tos']
+
