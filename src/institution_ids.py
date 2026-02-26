d68b400f70108959b1d3db409c06e13017c837d7
Peter J Gleckler <u7510@login39.chn.perlmutter.nersc.gov>
2026-02-25 17:49:56 -0800

    RC added

--- src/institution_ids.py
+++ src/institution_ids.py
@@ -9,6 +9,7 @@ institution_id['institution_id']['MRI'] = 'Meteorological Research Institute, Ts
 institution_id['institution_id']['NASA-JPL'] = 'NASA Jet Propulsion Laboratory, Pasadena, CA 91109, USA'
 institution_id['institution_id']['NASA-GSFC'] = 'NASA Goddard Space Flight Center, Greenbelt MD, USA'
 institution_id['institution_id']['NASA-LaRC'] = 'NASA-LaRC (Langley Research Center) Hampton, Va'
+institution_id['institution_id']['NASA-GMAO'] = 'NASA-GMAO (NASA Global Modeling and Assimilation Office),Greenbelt MD, USA'
 institution_id['institution_id']['NOAA-NCEI'] = 'NOAA National Centers for Environmental Information, Asheville, NC 28801, USA'
 institution_id['institution_id']['NOAA-ESRL-PSD'] = 'NOAA Earth System Research Laboratory, Physical Sciences Division, Boulder, CO 80305, USA'
 institution_id['institution_id']['PCMDI'] = 'Program for Climate Model Diagnosis and Intercomparison, Lawrence Livermore National Laboratory, Livermore, CA 94550, USA'
--- src/source_ids.py
+++ src/source_ids.py
@@ -1,18 +1,6 @@
 source_id = {}
 source_id['source_id'] = {}
 
-key = 'CCI-SWE-v4-0'
-source_id['source_id'][key] = {}
-source_id['source_id'][key]['source_name'] = 'CCI SWE' 
-source_id['source_id'][key]['source_label'] = 'CCI SWE'
-source_id['source_id'][key]['release_year'] = '2025'
-source_id['source_id'][key]['source_description'] = 'Snow Water Equivalent (SWE) from the ESA SNOW CCI Climate Data Record version 4.0'
-source_id['source_id'][key]['source_version_number'] = 'v4.0'
-source_id['source_id'][key]['institution_id'] = 'FMI'
-source_id['source_id'][key]['region'] = ['global_land']
-source_id['source_id'][key]['source_type'] = 'satellite_blended'
-source_id['source_id'][key]['source_variables'] = ['sweLut']
-
 key = 'NOAA-NCEI-OISST-v2-1'
 source_id['source_id'][key] = {}
 source_id['source_id'][key]['source_name'] = 'NOAA-NCEI OISST v2.1'
@@ -25,15 +13,15 @@ source_id['source_id'][key]['region'] = ['global_ocean']
 source_id['source_id'][key]['source_type'] = 'satellite_blended'
 source_id['source_id'][key]['source_variables'] = ['tos']
 
-key = 'NOAA-NCEI-ERSSTv6'
+key = 'MERRA-2'
 source_id['source_id'][key] = {}
-source_id['source_id'][key]['source_name'] = 'NOAA-NCEI ERSSTv6'
-source_id['source_id'][key]['source_label'] = 'NOAA-NCEI ERSSTv6'
+source_id['source_id'][key]['source_name'] = 'MERRA'
+source_id['source_id'][key]['source_label'] = 'MERRA'
 source_id['source_id'][key]['release_year'] = '2025'
-source_id['source_id'][key]['source_description'] = 'NOAA-NCEI Extended Reconstructed Sea Surface Temperature (ERSST)'
-source_id['source_id'][key]['source_version_number'] = 'v6'
-source_id['source_id'][key]['institution_id'] = 'NOAA-NCEI'
-source_id['source_id'][key]['region'] = ['global_ocean']
-source_id['source_id'][key]['source_type'] = 'satellite_blended'
-source_id['source_id'][key]['source_variables'] = ['tos']
+source_id['source_id'][key]['source_description'] = 'Modern-Era Retrospective analysis for Research and Applications'
+source_id['source_id'][key]['source_version_number'] = '2'
+source_id['source_id'][key]['institution_id'] = 'NASA-GMAO'
+source_id['source_id'][key]['region'] = ['global']
+source_id['source_id'][key]['source_type'] = 'reanalysis'
+source_id['source_id'][key]['source_variables'] = ['psl','tas','uas','vas','huss','ua','va','ta','zg','hus']
 
