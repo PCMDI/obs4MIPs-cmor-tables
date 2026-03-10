0582c7f80019c98c95f5126b5dccda12696c7c10
Jingbo Wu <jw2531@login20.chn.perlmutter.nersc.gov>
2026-03-10 06:35:45 -0700

    adding MEERA2 and other five directories for CONUS products under NASA-GISS

---
+++ inputs/NASA-GISS/NASA-GMAO/MERRA-2/runCmor_MERRA2_ASM.py
@@ -0,0 +1,92 @@
+import cmor
+import xcdat as xc
+import numpy as np
+import glob,os,sys
+import cftime
+from datetime import datetime
+sys.path.append("../../../../inputs/misc/") # Path to obs4MIPsLib
+import obs4MIPsLib
+
+def has_bounds(ds, names):
+    return any(n in ds.variables or n in ds.coords for n in names)
+
+#%% User provided input
+cmorTable = '../../../../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx
+#inputJson = 'MERRA2-monthly-asm.json' ; # Update contents of this file to set your global_attributes
+inputJson = 'test-asm.json' ; # Update contents of this file to set your global_attributes
+inputFilePath = '/global/cfs/projectdirs/m4581/obs4MIPs/obs4MIPs_input/NASA-GMAO/MERRA-2/monthly/2d'
+inputVarName = ['T2M','U2M','V2M','QV2M']
+outputVarName = ['tas','uas','vas','huss']  
+outputUnits = ['K','m s-1','m s-1','1'] 
+run_version = "v" + datetime.now().strftime("%Y%m%d") # fixed for entire run
+cmor_missing = np.float32(1.0e20)
+
+### BETTER IF THE USER DOES NOT CHANGE ANYTHING BELOW THIS LINE..
+
+for year in range(1980, 2027):  # put the years you want to process here
+    inputFiles = glob.glob(f"{inputFilePath}/MERRA2_???.instM_2d_asm_Nx.{year}??.nc4.nc4")
+    if len(inputFiles) == 0:
+        continue
+
+    # Open and read input netcdf files
+    # Process variable (with time axis)
+    inputFiles.sort(key=lambda f: datetime.strptime(f.split('.')[2], "%Y%m"))
+    f = xc.open_mfdataset(inputFiles, mask_and_scale=True, decode_times=True, use_cftime=True, combine='nested', concat_dim='time')
+    if not has_bounds(f, ["time_bnds", "time_bounds"]):
+        f = f.bounds.add_bounds("T")
+    if not has_bounds(f, ["lat_bnds", "lat_bounds"]):
+        f = f.bounds.add_bounds("Y")
+    if not has_bounds(f, ["lon_bnds", "lon_bounds"]):
+        f = f.bounds.add_bounds("X")
+
+    lat = f.lat.values
+    lon = f.lon.values
+    lat_bnds = f.get("lat_bnds", f.get("lat_bounds")).values
+    lon_bnds = f.get("lon_bnds", f.get("lon_bounds")).values
+
+    t_units = f.time.attrs.get("units") or f.time.encoding.get("units")
+    calendar = f.time.attrs.get("calendar") or f.time.encoding.get("calendar", "standard")
+    time = cftime.date2num(f.time.values, units=t_units, calendar=calendar).astype("float64")
+    time_bnds = cftime.date2num(f.get("time_bnds", f.get("time_bounds")).values, units=t_units, calendar=calendar).astype("float64")
+
+    for fi in range(len(inputVarName)): #looping over varaibles
+        d = f[inputVarName[fi]]
+    
+        #%% Initialize and run CMOR
+        cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4) #,logfile='cmorLog.txt')
+        cmor.dataset_json(inputJson)
+        cmor.set_cur_dataset_attribute("version", run_version)
+        cmor.load_table(cmorTable)
+    
+        axes = [
+            {"table_entry": "time", "units": t_units},
+            {"table_entry": "latitude", "units": "degrees_north",
+             "coord_vals": lat, "cell_bounds": lat_bnds},
+            {"table_entry": "longitude", "units": "degrees_east",
+             "coord_vals": lon, "cell_bounds": lon_bnds},
+        ]
+        axisIds = [cmor.axis(**ax) for ax in axes]
+    
+        # Setup units and create variable to write using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
+        varid = cmor.variable(outputVarName[fi],outputUnits[fi],axisIds,missing_value=cmor_missing)
+        values = np.array(d.values,np.float32)
+        fill = getattr(d, "_FillValue", None)
+        mask = ~np.isfinite(values) | (values == fill)
+        values = np.where(mask, cmor_missing, values) #convert to kg m-2 s-1
+    
+        # Append valid_min and valid_max to variable before writing using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
+        cmor.set_variable_attribute(varid,'valid_min','f',float(d.valid_range[0]))
+        cmor.set_variable_attribute(varid,'valid_max','f',float(d.valid_range[-1]))
+    
+        # Provenance info - produces global attribute <obs4MIPs_GH_Commit_ID> 
+        git_commit_number = obs4MIPsLib.get_git_revision_hash()
+        path_to_code = os.getcwd().split('obs4MIPs-cmor-tables')[1]
+        full_git_path = f"https://github.com/PCMDI/obs4MIPs-cmor-tables/tree/{git_commit_number}/{path_to_code}"
+        cmor.set_cur_dataset_attribute("processing_code_location",f"{full_git_path}")
+    
+        # Prepare variable for writing, then write and close file - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
+        cmor.set_deflate(varid,1,1,1) ; # shuffle=1,deflate=1,deflate_level=1 - Deflate options compress file data
+        cmor.write(varid,values,time_vals=time,time_bnds=time_bnds) ; # Write variable with time axis
+        f.close()
+        cmor.close()
+        print(f"File written for {fi}_{year}")
---
+++ inputs/NASA-GISS/OSU/PRISM/runCMOR-mon-PRISM.py
@@ -0,0 +1,87 @@
+import cmor
+import xarray as xr
+import numpy as np
+import glob,os,sys
+import cftime
+from datetime import datetime
+sys.path.append("../../../../../inputs/misc/") # Path to obs4MIPsLib and code to fix times
+import obs4MIPsLib
+
+def has_bounds(ds, names):
+    return any(n in ds.variables or n in ds.coords for n in names)
+
+#%% User provided input
+cmorTable = '../../../../../Tables/obs4MIPs_Amon.json' ; # Aday,Amon,Lmon,Omon,SImon,fx,monNobs,monStderr - Load target table, axis info (coordinates, grid*) and CVs
+inputJson = 'PRISM_OSU_inputs.json' ; # Update contents of this file to set your global_attributes
+inputFilePath = '/global/cfs/projectdirs/m4581/obs4MIPs/obs4MIPs_input/OSU/PRISM/monthly/processed_final' 
+inputVarName = ['PPT','TMEAN','TMAX','TMIN']
+inputUnits = ['kg m-2 s-1','K','K','K']  #input units same as required (?) output units, no unit conversion needed
+outputVarName = ['pr','tas','tasmax','tasmin']
+outputUnits = ['kg m-2 s-1','K','K','K']
+valid_min=[0.,230.,230.,230.]            #referenced from the actual input data values
+valid_max=[0.002,330.,330.,330.]         #referenced from the actual input data values
+run_version = "v" + datetime.now().strftime("%Y%m%d") # fixed for entire run
+cmor_missing = np.float32(1.0e20)
+
+for fi in range(len(inputVarName)): #looping over varaibles
+    inputFiles = f"{inputFilePath}/PRISM_monthly_{inputVarName[fi]}_1981-2005.nc"
+    if len(inputFiles) == 0:
+        continue
+
+    ##xarray used to solve inconsistency btw time and time_bnds, specifically for this dataset
+    f = xr.open_dataset(inputFiles, mask_and_scale=True, decode_times=False)
+    time_coder = xr.coders.CFDatetimeCoder(use_cftime=True)
+    f_decode = xr.decode_cf(f, decode_times=time_coder)
+
+    d = f[inputVarName[fi]]
+
+    lat = f.lat.values
+    lon = f.lon.values
+    lat_bnds = f.get("lat_bnds", f.get("lat_bounds")).values[0, :, :]
+    lon_bnds = f.get("lon_bnds", f.get("lon_bounds")).values[0, :, :]
+
+    t_units = f.time.attrs.get("units") or f.time.encoding.get("units")
+    calendar = f.time.attrs.get("calendar") or f.time.encoding.get("calendar", "standard")
+    time = cftime.date2num(f_decode.time.values, units=t_units, calendar=calendar).astype("float64")
+    time_bnds = cftime.date2num(f_decode.get("time_bnds", f_decode.get("time_bounds")).values, units=t_units, calendar=calendar).astype("float64")
+
+    #%% Initialize and run CMOR
+    # For more information see https://cmor.llnl.gov/mydoc_cmor3_api/
+    cmor.setup(inpath='./',netcdf_file_action=cmor.CMOR_REPLACE_4) #,logfile='cmorLog.txt')
+    cmor.dataset_json(inputJson)
+    cmor.set_cur_dataset_attribute("version", run_version)
+    cmor.load_table(cmorTable)
+
+    axes = [
+        {"table_entry": "time", "units": t_units},
+        {"table_entry": "latitude", "units": "degrees_north",
+         "coord_vals": lat, "cell_bounds": lat_bnds},
+        {"table_entry": "longitude", "units": "degrees_east",
+         "coord_vals": lon, "cell_bounds": lon_bnds},
+    ]
+    axisIds = [cmor.axis(**ax) for ax in axes]
+
+
+    # Setup units and create variable to write using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
+    varid = cmor.variable(outputVarName[fi],outputUnits[fi],axisIds,missing_value=cmor_missing)
+    values = np.array(d.values,np.float32)
+    fill = getattr(d, "_FillValue", None)
+    mask = ~np.isfinite(values) | (values == fill)
+    values = np.where(mask, cmor_missing, values)
+
+    # Append valid_min and valid_max to variable before writing using cmor - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
+    cmor.set_variable_attribute(varid,'valid_min','f',valid_min)
+    cmor.set_variable_attribute(varid,'valid_max','f',valid_max)
+
+    # Provenance info
+    git_commit_number = obs4MIPsLib.get_git_revision_hash()
+    path_to_code = os.getcwd().split('obs4MIPs-cmor-tables')[1]
+    full_git_path = f"https://github.com/PCMDI/obs4MIPs-cmor-tables/tree/{git_commit_number}/{path_to_code.lstrip('/')}"
+    cmor.set_cur_dataset_attribute("processing_code_location",f"{full_git_path}")
+
+    # Prepare variable for writing, then write and close file - see https://cmor.llnl.gov/mydoc_cmor3_api/#cmor_set_variable_attribute
+    cmor.set_deflate(varid,1,1,1) ; # shuffle=1,deflate=1,deflate_level=1 - Deflate options compress file data
+    cmor.write(varid,values,time_vals=time,time_bnds=time_bnds) ; # Write variable with time axis
+    f.close()
+    cmor.close()
+    print(f"File written for {inputVarName[fi]}")
